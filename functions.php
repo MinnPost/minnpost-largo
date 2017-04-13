958f5a119514f27247ba5889f886b744665863e9
Jonathan Stegall <jstegall@minnpost.com>
2017-04-13 12:43:39 -0500

    made a quick plugin for migrating random things. starting with menus. this also has theme files for menu locations and starting to display menus

--- functions.php
+++ functions.php
@@ -8,16 +8,25 @@ function enqueue_parent_styles() {
    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.min.css' );
 }
 
-if ( ! function_exists( 'minnpost_posted_at' ) ) :
-	function minnpost_posted_at() {
-		//echo is_single() ? twentyseventeen_time_link() : twentyseventeen_time_link();
-		//twentyseventeen_edit_link();
-		if ( get_the_date( 'Y-m-d'  ) === date( 'Y-m-d', time() ) ) :
-			printf( __( get_the_time(), 'minnpost' ) );
-		else:
-			printf( __( get_the_date(), 'minnpost' ) );	
-		endif;
+if ( ! function_exists( 'minnpost_menus' ) ) :
+	function minnpost_menus() {
+		// Add Your Menu Locations
+		register_nav_menus(
+			array(
+				'footer_primary' => __( 'Footer Primary' ), 
+				'footer_secondary' => __( 'Footer Secondary' ),
+				'minnpost_network' => __( 'Network Menu' ),
+				'support_minnpost' => __( 'Support Menu' ),
+				'top_menu' => __( 'Top Menu' ),
+				'secondary_links' => __( 'Secondary' ),
+				'primary_links' => __( 'Primary' ),
+			)
+		);
+		unregister_nav_menu( 'menu-1' );
 	}
+
+	add_action( 'init', 'minnpost_menus' );
+
 endif;
 
 
