deaf0bf0b4be3f905df7ad4e68a5e259db9ffa80
Jonathan Stegall <jstegall@minnpost.com>
2017-06-26 15:42:39 -0500

    custom fields for categories

--- inc/custom-fields.php
+++ inc/custom-fields.php
@@ -55,11 +55,24 @@ Container::make( 'post_meta', 'Subtitle Settings' )->show_on_post_type( 'post' )
  */
 Container::make( 'term_meta', 'Category Properties' )->show_on_taxonomy( 'category' )->add_fields(
 	array(
-		Field::make( 'text', 'crb_deck', 'Deck' ),
-		Field::make( 'rich_text', 'crb_excerpt', 'Excerpt' ),
-		Field::make( 'rich_text', 'crb_sponsorship', 'Sponsorship' ),
-		Field::make( 'image', 'crb_thumbnail', 'Category Thumbnail' ),
-		Field::make( 'image', 'crb_main_image', 'Category Main Image' ),
-		Field::make( 'rich_text', 'crb_body', 'Body' ),
+		Field::make( 'rich_text', '_mp_category_excerpt', 'Excerpt' ),
+		Field::make( 'rich_text', '_mp_category_sponsorship', 'Sponsorship' ),
+		Field::make( 'image', '_mp_category_thumbnail_id', 'Category Thumbnail' ),
+		Field::make( 'image', '_mp_category_main_image_id', 'Category Main Image' ),
+		Field::make( 'rich_text', '_mp_category_body', 'Body' ),
 	)
 );
+
+if ( ! function_exists( 'remove_default_category_description' ) ) :
+	add_action('admin_head', 'remove_default_category_description');
+	function remove_default_category_description() {
+	    global $current_screen;
+	    if ( $current_screen->id == 'edit-category' ) { ?>
+			<script>
+			jQuery(function($) {
+	        	$('textarea#description, textarea#tag-description').closest('tr.form-field, div.form-field').remove();
+			});
+			</script>
+		<?php }
+	}
+endif;
