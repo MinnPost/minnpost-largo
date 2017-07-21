f15d3a8f965dda4b138e76ee25f5014e7fd69e56
Jonathan Stegall <jstegall@minnpost.com>
2017-07-21 10:09:28 -0500

    fix settings for newsletter content type

--- inc/custom-types.php
+++ inc/custom-types.php
@@ -7,19 +7,32 @@ if ( ! function_exists( 'create_newsletter' ) ) :
 	    register_post_type(
 			'newsletter',
 			array(
-				'label' => 'Newsletter',
+				'label' => __( 'Newsletter', 'minnpost-largo' ),
 				'labels' => array(
-				'name' => 'Newsletters',
-				'singular_name' => 'Newsletter',
-			),
-			'show_ui' => true,
-			'public' => true,
-			'supports' => array(
-				'title',
-				'editor'
-			),
-	    )
-	  );
+					'name' => __( 'Newsletters', 'minnpost-largo' ),
+					'singular_name' => __( 'Newsletter', 'minnpost-largo' ),
+					'menu_name' => __( 'Newsletters', 'minnpost-largo' ),
+					'name_admin_bar' => __( 'Newsletters', 'minnpost-largo' ),
+					'add_new' => __( 'Add New', 'minnpost-largo' ),
+					'add_new_item' => __( 'Add New Newsletter', 'minnpost-largo' ),
+					'new_item' => __( 'New Newsletter', 'minnpost-largo' ),
+					'edit_item' => __( 'Edit Newsletter', 'minnpost-largo' ),
+					'view_item' => __( 'View Newsletter', 'minnpost-largo' ),
+					'all_items' => __( 'All Newsletters', 'minnpost-largo' ),
+					'search_items' => __( 'Search Newsletters', 'minnpost-largo' ),
+					'parent_item_colon' => __( 'Parent Newsletters:', 'minnpost-largo' ),
+					'not_found' => __( 'No newsletters found.', 'minnpost-largo' ),
+					'not_found_in_trash' => __( 'No newsletters found in Trash.', 'minnpost-largo' ),
+				),
+				'show_ui' => true,
+				'public' => true,
+				'hierarchical' => true,
+				'supports' => array(
+					'title',
+					'editor',
+				),
+	    	)
+		);
 	}
 endif;
 
@@ -28,27 +41,30 @@ endif;
 add_action( 'init', 'cptui_register_my_cpts' );
 function cptui_register_my_cpts() {
 	$labels = array(
-		"name" => __( 'Tests', 'minnpost-largo' ),
-		"singular_name" => __( 'Test', 'minnpost-largo' ),
+		'name' => __( 'Tests', 'minnpost-largo' ),
+		'singular_name' => __( 'Test', 'minnpost-largo' ),
 	);
 	$args = array(
-		"label" => __( 'Tests', 'minnpost-largo' ),
-		"labels" => $labels,
-		"description" => "",
-		"public" => true,
-		"publicly_queryable" => true,
-		"show_ui" => true,
-		"show_in_rest" => false,
-		"rest_base" => "",
-		"has_archive" => false,
-		"show_in_menu" => true,
-		"exclude_from_search" => false,
-		"capability_type" => "post",
-		"map_meta_cap" => true,
-		"hierarchical" => false,
-		"rewrite" => array( "slug" => "test", "with_front" => true ),
-		"query_var" => true,
-		"supports" => array( "title", "editor", "thumbnail" ),
+		'label' => __( 'Tests', 'minnpost-largo' ),
+		'labels' => $labels,
+		'description' => '',
+		'public' => true,
+		'publicly_queryable' => true,
+		'show_ui' => true,
+		'show_in_rest' => false,
+		'rest_base' => '',
+		'has_archive' => false,
+		'show_in_menu' => true,
+		'exclude_from_search' => false,
+		'capability_type' => 'post',
+		'map_meta_cap' => true,
+		'hierarchical' => false,
+		'rewrite' => array(
+			'slug' => 'test',
+			'with_front' => true,
+		),
+		'query_var' => true,
+		'supports' => array( 'title', 'editor', 'thumbnail' ),
 	);
-	register_post_type( "test", $args );
+	register_post_type( 'test', $args );
 }
