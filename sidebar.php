f4226c0c83edf2d5a6219ca9d33c6fa8b2bb7821
Jonathan Stegall <jstegall@minnpost.com>
2017-08-10 14:57:18 -0500

    fix which ads show on the sidebars

--- sidebar.php
+++ sidebar.php
@@ -16,11 +16,27 @@
 	 <div class="m-ad-region m-ad-region-sidebar">
 		<?php do_action( 'acm_tag', 'Right1' ); ?>
 	</div>
+	<?php if ( is_archive() || is_single() ) : ?>
+		<?php
+		$i = 1;
+		while ( $i <= 10 ) : ?>
+			<div class="m-ad-region m-ad-region-sidebar">
+				<?php do_action( 'acm_tag', 'X' . str_pad( $i, 2, '0', STR_PAD_LEFT ) ); ?>
+			</div>
+		<?php
+		$i++;
+		endwhile;
+		?>
+	<?php endif; ?>
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
-	<div class="m-ad-region m-ad-region-sidebar">
-		<?php do_action( 'acm_tag', 'Middle' ); ?>
-	</div>
-	<div class="m-ad-region m-ad-region-sidebar">
-		<?php do_action( 'acm_tag', 'BottomRight' ); ?>
-	</div>
+	<?php if ( ! is_single() ) : ?>
+		<div class="m-ad-region m-ad-region-sidebar">
+			<?php do_action( 'acm_tag', 'Middle' ); ?>
+		</div>
+	<?php endif; ?>
+	<?php if ( is_home() ) : ?>
+		<div class="m-ad-region m-ad-region-sidebar">
+			<?php do_action( 'acm_tag', 'BottomRight' ); ?>
+		</div>
+	<?php endif; ?>
 </aside><!-- #secondary -->
