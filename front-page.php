18d5ef009c1f7a8b7921ce3be5021c4007c45475
Jonathan Stegall <jstegall@minnpost.com>
2017-08-18 13:31:52 -0500

    fix homepage zones in the event that they are empty. this does happen sometimes.

--- front-page.php
+++ front-page.php
@@ -13,52 +13,51 @@ get_header(); ?>
 		
 	<div id="primary" class="m-layout-primary o-homepage-listing">
 		<main id="main" class="site-main m-homepage-zones" role="main">
-
-			<section class="m-zone m-zone-homepage-top m-archive m-archive-top">
-				<?php
-				if ( function_exists( 'z_get_zone' ) ) {
-					$the_query = z_get_zone_query( 'homepage-top' );
-				}
-				if ( $the_query->have_posts() ) :
+			<?php
+			if ( function_exists( 'z_get_zone' ) ) {
+				$the_query = z_get_zone_query( 'homepage-top' );
+			}
+			if ( $the_query->have_posts() ) : ?>
+				<section class="m-zone m-zone-homepage-top m-archive m-archive-top">
+					<?php
 					while ( $the_query->have_posts() ) : $the_query->the_post();
 						get_template_part( 'template-parts/content', 'top' ); // content-top
 					endwhile;
-				else : // I'm not sure it's possible to have no posts when this page is shown
-					get_template_part( 'template-parts/content', 'none' );
-				endif; ?>
-			</section>
-
-			<section class="m-zone m-zone-homepage-middle m-archive m-archive-middle">
-				<?php
-				if ( function_exists( 'z_get_zone' ) ) {
-					$the_query = z_get_zone_query( 'homepage-middle' );
-				}
-				if ( $the_query->have_posts() ) :
+					?>
+				</section>
+			<?php endif; ?>
+			
+			<?php
+			if ( function_exists( 'z_get_zone' ) ) {
+				$the_query = z_get_zone_query( 'homepage-middle' );
+			}
+			if ( $the_query->have_posts() ) : ?>
+				<section class="m-zone m-zone-homepage-middle m-archive m-archive-middle">
+					<?php
 					while ( $the_query->have_posts() ) : $the_query->the_post();
 						get_template_part( 'template-parts/content', 'middle' ); // content-middle
 					endwhile;
-				else : // I'm not sure it's possible to have no posts when this page is shown
-					get_template_part( 'template-parts/content', 'none' );
-				endif; ?>
-			</section>
+					?>
+				</section>
+			<?php endif; ?>
 
 			<div class="m-ad-region m-ad-region-home">
 				<?php do_action( 'acm_tag', 'Middle3' ); ?>
 			</div>
 
-			<section class="m-zone m-zone-homepage-bottom m-archive m-archive-excerpt">
-				<?php
-				if ( function_exists( 'z_get_zone' ) ) {
-					$the_query = z_get_zone_query( 'homepage-bottom' );
-				}
-				if ( $the_query->have_posts() ) :
+			<?php
+			if ( function_exists( 'z_get_zone' ) ) {
+				$the_query = z_get_zone_query( 'homepage-bottom' );
+			}
+			if ( $the_query->have_posts() ) : ?>
+				<section class="m-zone m-zone-homepage-bottom m-archive m-archive-excerpt">
+					<?php
 					while ( $the_query->have_posts() ) : $the_query->the_post();
 						get_template_part( 'template-parts/content', 'excerpt' ); // content-excerpt
 					endwhile;
-				else : // I'm not sure it's possible to have no posts when this page is shown
-					get_template_part( 'template-parts/content', 'none' );
-				endif; ?>
-			</section>
+					?>
+				</section>
+			<?php endif; ?>
 
 		</main><!-- #main -->
 
