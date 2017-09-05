e6def9c379c04bb19648b3b613e5220f713da6e1
Jonathan Stegall <jstegall@minnpost.com>
2017-09-05 10:18:23 -0500

    add some methods we need to extras.php

--- inc/extras.php
+++ inc/extras.php
@@ -15,3 +15,25 @@ if ( ! function_exists( 'minnpost_remove_comment_support' ) ) :
 	add_action( 'init', 'minnpost_remove_comment_support', 100 );
 
 endif;
+
+
+if ( ! function_exists( 'is_post_type' ) ) :
+	function is_post_type( $type ) {
+	    global $wp_query;
+	    if ( get_post_type( $wp_query->post->ID ) === $type ) {
+	    	return true;
+	    }
+	    return false;
+	}
+endif;
+
+if ( ! function_exists( 'minnpost_post_thumbnail_sizes_attr' ) ) :
+	add_filter( 'wp_get_attachment_image_attributes', 'minnpost_post_thumbnail_sizes_attr', 10 , 3 );
+	function minnpost_post_thumbnail_sizes_attr( $attr = array(), $attachment, $size = '' ) {
+		if ( 'post-thumbnail' === $size ) {
+			is_active_sidebar( 'sidebar-1' ) && $attr['sizes'] = '(max-width: 709px) 85vw, (max-width: 909px) 67vw, (max-width: 984px) 60vw, (max-width: 1362px) 62vw, 840px';
+			! is_active_sidebar( 'sidebar-1' ) && $attr['sizes'] = '(max-width: 709px) 85vw, (max-width: 909px) 67vw, (max-width: 1362px) 88vw, 1200px';
+		}
+		return $attr;
+	}
+endif;
