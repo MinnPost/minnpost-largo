66377ca2e3c515c661d9c07107a73fde4612d7d2
Jonathan Stegall <jstegall@minnpost.com>
2017-09-18 14:04:45 -0500

    docs for email templates

--- footer-newsletter.php
+++ footer-newsletter.php
@@ -1,2 +1,15 @@
+<?php
+/**
+ * The template for displaying the email footer
+ *
+ * Contains the closing of email newsletter stuff
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package MinnPost Largo
+ */
+
+?>
+
 	</body>
 </html>
--- template-parts/content-newsletter.php
+++ template-parts/content-newsletter.php
@@ -1,1 +1,11 @@
+<?php
+/**
+ * Template part for displaying posts inside newsletter templates
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package MinnPost Largo
+ */
+
+?>
 this
