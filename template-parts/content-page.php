478d13e8d98726ddd2cab2519fd4b5172f74e0e4
Jonathan Stegall <jstegall@minnpost.com>
2017-09-28 11:44:37 -0500

    basic content template for pages

---
+++ template-parts/content-page.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Template part for displaying pages
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package MinnPost Largo
+ */
+
+?>
+
+<article id="page-<?php the_ID(); ?>" <?php post_class( 'm-page' ); ?>>
+
+	<header class="m-entry-header">
+		<?php
+		if ( is_singular() ) :
+			the_title( '<h1 class="a-entry-title">', '</h1>' );
+		else :
+			the_title( '<h3 class="a-entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+		endif;
+		?>
+
+	</header><!-- .m-entry-header -->
+
+	<div class="m-entry-content">
+		<?php the_content(); ?>
+	</div><!-- .entry-content -->
+
+</article><!-- #page-## -->
