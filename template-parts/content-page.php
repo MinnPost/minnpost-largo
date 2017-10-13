313289dacf148192d38ef714a16b52c98ca6b98a
Jonathan Stegall <jstegall@minnpost.com>
2017-10-13 14:11:50 -0500

    add an edit link to pages

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -18,6 +18,15 @@
 		else :
 			the_title( '<h3 class="a-entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
+		edit_post_link(
+			sprintf(
+				/* translators: %s: Name of current post */
+				esc_html__( 'Edit %s', 'minnpost-largo' ),
+				the_title( '<span class="screen-reader-text">"', '"</span>', false )
+			),
+			'<span class="edit-link">',
+			'</span>'
+		);
 		?>
 
 	</header><!-- .m-entry-header -->
