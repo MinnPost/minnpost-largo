cb9aa07982264bdd1f6b2320d29514dc1573c523
Jonathan Stegall <jstegall@minnpost.com>
2017-11-28 12:21:12 -0600

    make a 404 template and use it to load 'page-not-found'

---
+++ 404.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * The template for displaying 404 pages (not found)
+ *
+ * @link https://codex.wordpress.org/Creating_an_Error_404_Page
+ *
+ * @package MinnPost Largo
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="m-layout-primary">
+		<main id="main" class="site-main" role="main">
+
+			<?php
+			$query = new WP_Query(
+				array(
+					'post_type' => 'page',
+					'pagename' => 'page-not-found',
+				)
+			);
+			while ( $query->have_posts() ) : $query->the_post();
+				get_template_part( 'template-parts/content', 'page' );
+			endwhile; // End of the loop.
+			wp_reset_postdata();
+			?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -11,9 +11,9 @@
 
 <article id="page-<?php the_ID(); ?>" <?php post_class( 'm-page' ); ?>>
 
-	<header class="m-entry-header<?php if ( is_singular() ) { ?> m-entry-header-singular<?php } ?>">
+	<header class="m-entry-header<?php if ( is_singular() || is_404() ) { ?> m-entry-header-singular<?php } ?>">
 		<?php
-		if ( is_singular() ) :
+		if ( is_singular() || is_404() ) :
 			the_title( '<h1 class="a-entry-title">', '</h1>' );
 		else :
 			the_title( '<h3 class="a-entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
@@ -42,6 +42,6 @@
 
 	<div class="m-entry-content<?php echo $custom_class; ?>">
 		<?php the_content(); ?>
-	</div><!-- .entry-content -->
+	</div><!-- .m-entry-content -->
 
 </article><!-- #page-## -->
