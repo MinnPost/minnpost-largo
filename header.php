86b762ae29ce21d62e30c91f3451d5526f60dce0
Jonathan Stegall <jstegall@minnpost.com>
2017-11-30 21:48:02 -0600

    update what modernizr is doing

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
  */
 
 ?><!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<html class="no-js" <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
