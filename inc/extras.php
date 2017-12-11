e40a36d2fa2e56220b7b76997e14b312ffc2ff0c
Jonathan Stegall <jstegall@minnpost.com>
2017-12-11 16:56:52 -0600

    remove some <head> stuff we don't need

--- inc/extras.php
+++ inc/extras.php
@@ -290,3 +290,15 @@ if ( ! function_exists( 'minnpost_document_title_separator' ) ) :
 		return $sep;
 	}
 endif;
+
+if ( ! function_exists( 'minnpost_remove_head_hooks' ) ) :
+	function minnpost_remove_head_hooks() {
+		add_filter( 'feed_links_show_comments_feed', '__return_false' );
+		remove_action( 'wp_head','feed_links_extra', 3 );
+		remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+		remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+		remove_action( 'wp_print_styles', 'print_emoji_styles' );
+		remove_action( 'admin_print_styles', 'print_emoji_styles' );
+	}
+	minnpost_remove_head_hooks();
+endif;
