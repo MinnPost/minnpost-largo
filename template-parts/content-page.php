6b17f68b769404c6859b441739a4cd8a5c434aa6
Jonathan Stegall <jstegall@minnpost.com>
2017-12-21 15:41:41 -0600

    use the minnpost_edit_link on page template

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -18,15 +18,7 @@
 		else :
 			the_title( '<h3 class="a-entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
-		edit_post_link(
-			sprintf(
-				/* translators: %s: Name of current post */
-				esc_html__( 'Edit %s', 'minnpost-largo' ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			),
-			'<span class="edit-link">',
-			'</span>'
-		);
+		minnpost_edit_link();
 		?>
 
 	</header><!-- .m-entry-header -->
