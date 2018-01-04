cb7b9ce6710d6bc1d330208755a37156cd3c2104
Jonathan Stegall <jstegall@minnpost.com>
2018-01-03 22:42:07 -0600

    get rid of that test custom post type

--- inc/custom-types.php
+++ inc/custom-types.php
@@ -42,36 +42,3 @@ if ( ! function_exists( 'create_newsletter' ) ) :
 	}
 
 endif;
-
-
-// that temporary test item
-add_action( 'init', 'cptui_register_my_cpts' );
-function cptui_register_my_cpts() {
-	$labels = array(
-		'name' => __( 'Tests', 'minnpost-largo' ),
-		'singular_name' => __( 'Test', 'minnpost-largo' ),
-	);
-	$args = array(
-		'label' => __( 'Tests', 'minnpost-largo' ),
-		'labels' => $labels,
-		'description' => '',
-		'public' => true,
-		'publicly_queryable' => true,
-		'show_ui' => true,
-		'show_in_rest' => false,
-		'rest_base' => '',
-		'has_archive' => false,
-		'show_in_menu' => true,
-		'exclude_from_search' => false,
-		'capability_type' => 'post',
-		'map_meta_cap' => true,
-		'hierarchical' => false,
-		'rewrite' => array(
-			'slug' => 'test',
-			'with_front' => true,
-		),
-		'query_var' => true,
-		'supports' => array( 'title', 'editor', 'thumbnail' ),
-	);
-	register_post_type( 'test', $args );
-}
