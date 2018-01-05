71f57c201a3ec60bc6e11b71d8553db4add92fc2
Jonathan Stegall <jstegall@minnpost.com>
2018-01-05 10:49:29 -0600

    put the author excerpt into a template part

--- single.php
+++ single.php
@@ -16,6 +16,7 @@ get_header(); ?>
 		while ( have_posts() ) : the_post();
 
 			get_template_part( 'template-parts/content', get_post_format() );
+			get_template_part( 'template-parts/author', 'excerpt' ); // author-excerpt
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
