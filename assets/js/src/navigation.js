cb232dde0ce894e611d5a3aab2aab80330fb3341
Jonathan Stegall <jstegall@minnpost.com>
2018-02-02 15:13:44 -0600

    don't break the links though

--- assets/js/src/navigation.js
+++ assets/js/src/navigation.js
@@ -111,7 +111,7 @@ var $ = window.jQuery;
 $(document).ready(function() {
 	// hide menu
 	if ($('#user-account-access ul').length > 0 ) {
-		$('#user-account-access li a').on( 'click', function(event) {
+		$('#user-account-access > li a').on( 'click', function(event) {
 			$('#user-account-access ul').toggleClass( 'visible' );
 			event.preventDefault();
 		});
