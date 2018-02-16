1072c9d83686164b5ffe1652ecd0ce60e2d280dc
Jonathan Stegall <jstegall@minnpost.com>
2018-02-16 11:45:10 -0600

    fix style/script queue

--- inc/custom-scripts-and-styles.php
+++ inc/custom-scripts-and-styles.php
@@ -5,11 +5,21 @@
  * @package MinnPost Largo
  */
 
-if ( ! function_exists( 'minnpost_largo_load_custom_scripts' ) ) :
-	add_action( 'wp_enqueue_scripts', 'minnpost_largo_load_custom_scripts' );
-	function minnpost_largo_load_custom_scripts() {
-		wp_enqueue_script( 'modernizr', get_theme_file_uri() . '/assets/js/modernizr-custom.js', array(), '1.0', true );
-		wp_enqueue_script( 'minnpost', get_theme_file_uri() . '/assets/js/minnpost.js', array( 'jquery', 'modernizr' ), '1.0', false );
+if ( ! function_exists( 'minnpost_largo_add_remove_scripts' ) ) :
+	add_action( 'wp_enqueue_scripts', 'minnpost_largo_add_remove_scripts' );
+	function minnpost_largo_add_remove_scripts() {
+		// add
+		$suffix = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min';
+		wp_enqueue_script( 'modernizr', get_theme_file_uri() . '/assets/js/modernizr-custom' . $suffix . '.js', array(), '1.0', true );
+		wp_enqueue_script( 'minnpost', get_theme_file_uri() . '/assets/js/minnpost' . $suffix . '.js', array( 'jquery', 'modernizr' ), filemtime( get_theme_file_path() . '/assets/js/minnpost' . $suffix . '.js' ), false );
+		// localize
+		$params = array(
+			'ajaxurl' => admin_url( 'admin-ajax.php' ),
+		);
+		wp_localize_script( 'minnpost', 'params', $params );
+		// remove
+		wp_dequeue_script( 'largo-navigation' );
+		wp_dequeue_script( 'popular-widget' );
 	}
 endif;
 
@@ -24,21 +34,18 @@ if ( ! function_exists( 'load_custom_wp_admin_style' ) ) :
 	}
 endif;
 
-if ( ! function_exists( 'minnpost_largo_remove_scripts' ) && ! function_exists( 'minnpost_largo_add_remove_styles' ) ) :
-	add_action( 'wp_enqueue_scripts', 'minnpost_largo_remove_scripts', 10 );
-	function minnpost_largo_remove_scripts() {
-		wp_dequeue_script( 'largo-navigation' );
-		wp_dequeue_script( 'popular-widget' );
-	}
+if ( ! function_exists( 'minnpost_largo_add_remove_styles' ) ) :
 	add_action( 'wp_print_styles', 'minnpost_largo_add_remove_styles', 10 );
 	function minnpost_largo_add_remove_styles() {
+		// add
 		$suffix = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min';
+		wp_enqueue_style( 'minnpost-style', get_theme_file_uri() . '/style' . $suffix . '.css', array(), filemtime( get_theme_file_path() . '/style' . $suffix . '.css' ), false );
+		// remove
 		wp_dequeue_style( 'largo-style' );
 		wp_dequeue_style( 'media-credit' );
 		wp_dequeue_style( 'widgetopts-styles' );
 		wp_dequeue_style( 'minnpost-nimbus' );
 		wp_dequeue_style( 'minnpost-donation-progress-widget' );
-		wp_enqueue_style( 'minnpost-style', get_theme_file_uri() . '/style' . $suffix . '.css', array(), filemtime( get_template_directory() . '/style' . $suffix . '.css' ), false );
 		wp_dequeue_style( 'popular-widget' );
 		wp_dequeue_style( 'creativ_sponsor' );
 	}
