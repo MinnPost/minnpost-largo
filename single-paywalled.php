4feb4d04e9c1d4f285a69e91352a6d4b3a4e2ecd
Jonathan Stegall <jstegall@minnpost.com>
2018-02-26 12:01:05 -0600

    add template stuff for paywalled items

---
+++ single-paywalled.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The template for displaying all paywalled single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package MinnPost Largo
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="m-layout-primary">
+		<main id="main" class="site-main" role="main">
+			<?php
+			while ( have_posts() ) :
+				the_post();
+				get_template_part( 'template-parts/content-paywalled', get_post_format() );
+			endwhile; // End of the loop.
+			?>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
