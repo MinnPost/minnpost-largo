7df13f4f82fead0ea255369e24efadfe9ba4f024
Jonathan Stegall <jstegall@minnpost.com>
2018-03-12 10:16:56 -0500

    formatting for custom settings inc

--- inc/custom-settings.php
+++ inc/custom-settings.php
@@ -2,29 +2,88 @@
 /**
  * Create custom admin settings fields
  *
- *
  * @package MinnPost Largo
  */
 
 $new_minnpost_setting = new Minnpost_General_Setting();
 
+/**
+* This class handles additional settings fields for this theme.
+*
+* This includes:
+* default image URL for sharing
+* Site footer message
+* Email address for site emails
+* Name of sender for site emails
+*
+*/
 class Minnpost_General_Setting {
+
+	/**
+	* Admin hook to register the fields
+	*
+	*/
 	function __construct() {
-		add_filter( 'admin_init' , array( $this, 'register_fields' ) );
+		add_filter( 'admin_init', array( $this, 'register_fields' ) );
 	}
+
+	/**
+	* Use the settings api to create the basic text fields
+	*
+	*/
 	function register_fields() {
+
+		// default image url
 		register_setting( 'general', 'default_image_url', 'esc_attr' );
-		add_settings_field( 'default_image_url', '<label for="default_image_url">' . __( 'Default Image URL' , 'minnpost-largo' ) . '</label>' , array( $this, 'default_image_html' ) , 'general' );
+		add_settings_field(
+			'default_image_url',
+			'<label for="default_image_url">' . __( 'Default Image URL', 'minnpost-largo' ) . '</label>',
+			array(
+				$this,
+				'default_image_html',
+			),
+			'general'
+		);
 
+		// site footer message
 		register_setting( 'general', 'site_footer_message', 'esc_attr' );
-		add_settings_field( 'site_footer_message', '<label for="site_footer_message">' . __( 'Site Footer Message' , 'minnpost-largo' ) . '</label>' , array( $this, 'footer_message_html' ) , 'general' );
+		add_settings_field(
+			'site_footer_message',
+			'<label for="site_footer_message">' . __( 'Site Footer Message', 'minnpost-largo' ) . '</label>',
+			array(
+				$this,
+				'footer_message_html',
+			),
+			'general'
+		);
 
+		// email address for site emails
 		register_setting( 'general', 'site_email_from', 'sanitize_email' );
-		add_settings_field( 'site_email_from', '<label for="site_email_from">' . __( 'Email Sending Address' , 'minnpost-largo' ) . '</label>' , array( $this, 'email_sending_address_html' ) , 'general' );
+		add_settings_field(
+			'site_email_from',
+			'<label for="site_email_from">' . __( 'Email Sending Address', 'minnpost-largo' ) . '</label>',
+			array(
+				$this,
+				'email_sending_address_html',
+			),
+			'general'
+		);
 
+		// sender name for site emails
 		register_setting( 'general', 'site_email_from_name', 'esc_attr' );
-		add_settings_field( 'site_email_from_name', '<label for="site_email_from_name">' . __( 'Email Sending Name' , 'minnpost-largo' ) . '</label>' , array( $this, 'email_sending_name_html' ) , 'general' );
+		add_settings_field(
+			'site_email_from_name',
+			'<label for="site_email_from_name">' . __( 'Email Sending Name', 'minnpost-largo' ) . '</label>',
+			array(
+				$this,
+				'email_sending_name_html',
+			),
+			'general'
+		);
+
 	}
+
+	// handle the html for each of the above fields
 	function default_image_html() {
 		$value = get_option( 'default_image_url', '' );
 		echo '<input type="text" id="default_image_url" name="default_image_url" value="' . $value . '" />';
