487b40f0c4ad42ace134fc46410c68d83cd3c8ee
Jonathan Stegall <jstegall@minnpost.com>
2018-03-12 09:46:10 -0500

    formatting on custom-queries file

--- inc/custom-queries.php
+++ inc/custom-queries.php
@@ -2,17 +2,25 @@
 /**
  * Create custom wp queries
  *
- *
  * @package MinnPost Largo
+ *
  */
 
+/**
+* Change the post query used on category archive pages based on whether or not they have featured columns.
+* This arranges featured posts and not featured posts on those archives.
+*
+* @param object $query
+*
+* @return object $query
+*/
 if ( ! function_exists( 'custom_archive_query_vars' ) ) :
 	add_action( 'pre_get_posts', 'custom_archive_query_vars' );
 	function custom_archive_query_vars( $query ) {
 		if ( $query->is_archive() && ! is_admin() && $query->is_main_query() ) {
 			if ( is_category() ) {
 				$category_id = $query->get_queried_object_id();
-				$figure = minnpost_get_term_figure( $category_id );
+				$figure      = minnpost_get_term_figure( $category_id );
 				if ( '' === get_term_meta( $category_id, '_mp_category_body', true ) || '' === $figure ) {
 					$featured_num = 3;
 				} else {
