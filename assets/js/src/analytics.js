506fd9d41ee1bc13747eca01c126fbc5fc63ef2d
Jonathan Stegall <jstegall@minnpost.com>
2018-05-25 12:30:53 -0500

    analytics event for member content

--- assets/js/src/analytics.js
+++ assets/js/src/analytics.js
@@ -9,3 +9,16 @@ function mp_analytics_tracking_event( type, category, action, label, value ) {
 		return;
 	}
 }
+
+jQuery( document ).ready( function ( $ ) {
+	if ( 'undefined' !== typeof minnpost_membership_data && '' !== minnpost_membership_data.url_access_level ) {
+		var type = 'event';
+		var category = 'Member Content';
+		var label = location.pathname; // i think we could possibly put some grouping here, but we don't necessarily have access to one and maybe it's not worthwhile yet
+		var action = 'Blocked';
+		if ( true === minnpost_membership_data.current_user.can_access ) {
+			action = 'Shown';
+		}
+		mp_analytics_tracking_event( type, category, action, label );
+	}
+});
