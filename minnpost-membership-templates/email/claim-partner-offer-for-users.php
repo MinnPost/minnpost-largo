10703e728f50a91867fe9507a940857989a6eded
Jonathan Stegall <jstegall@minnpost.com>
2018-06-28 11:41:31 -0500

    html template for email we'll send to users when they claim an offer

---
+++ minnpost-membership-templates/email/claim-partner-offer-for-users.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<!--[if !mso]><!-->
+			<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<!--<![endif]-->
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>Welcome to MinnPost</title>
+		<style type="text/css" data-premailer="ignore">
+			.webkit {
+				max-width: 600px;
+			}
+			.ExternalClass {
+				width: 100%;
+			}
+			.ExternalClass, .ExternalClass p, .ExternalClass span, .ExternalClass font, .ExternalClass td, .ExternalClass div {
+				line-height: 100%;
+			}
+			
+			a[x-apple-data-detectors] {
+				color: #801019 !important;
+				text-decoration: none !important;
+				font-size: inherit !important;
+				font-family: inherit !important;
+				font-weight: normal !important;
+				line-height: inherit !important;
+			}
+		</style>
+
+		 <!--[if (gte mso 9)|(IE)]>
+			<style type="text/css" data-premailer="ignore">
+			.header table { font-size:1px; line-height:0; mso-margin-top-alt:1px; }
+			</style>
+			<![endif]-->
+
+	<style>body {
+margin: 0; padding: 0; min-width: 100%; background-color: #ffffff;
+}
+img {
+border: 0 none; height: auto; line-height: 100%; outline: none; text-decoration: none;
+}
+a:hover {
+color: #801019 !important; text-decoration: underline !important;
+}
+a:active {
+color: #801019 !important; text-decoration: underline !important;
+}
+a:visited {
+color: #801019 !important;
+}
+h3 a:active {
+color: #1A1818;
+}
+h5 a:active {
+color: #1A1818;
+}
+h3 a:visited {
+color: #1A1818;
+}
+h5 a:visited {
+color: #1A1818;
+}
+.content .button.read-story td td a:hover {
+color: #ffffff !important; text-decoration: none !important;
+}
+.content .button.read-story td td a:visited {
+color: #ffffff !important; text-decoration: none !important;
+}
+.content .button.read-story td td a:active {
+color: #ffffff !important; text-decoration: none !important;
+}
+@media only screen and (max-width: 600px) {
+	.one-column .column {
+		max-width: 90% !important; margin-right: auto !important; margin-left: auto !important;
+	}
+}
+</style></head>
+	<body style="min-width: 100%; margin: 0; padding: 0;" bgcolor="#ffffff">
+
+	<!-- limit to 50 characters; only display if there is a value -->
+	<span style="display: none !important; font-size: 0; color: #fff;"> <!-- gmail only takes display none if it has !important; outlook still doesn't care -->
+		Welcome to your MinnPost.com website account.
+	</span>
+
+	<center class="wrapper" style="width: 100%; table-layout: fixed; -webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%;">
+		<div class="webkit">
+			
+			<!--[if (gte mso 9)|(IE)]>
+				<table cellpadding="0" cellspacing="0" width="600" align="center">
+					<tr>
+						<td>
+						<![endif]-->
+				<table cellpadding="0" cellspacing="0" class="outer" align="center" style="border-spacing: 0; margin: 0 auto; padding: 0; font-family: Helvetica, Arial, Geneva, sans-serif; color: #1a1818; mso-table-lspace: 0pt; mso-table-rspace: 0pt; border-collapse: collapse; width: 100%; max-width: 600px;">
+					
+					<tr>
+						<td class="one-column header" style="border-collapse: collapse; font-size: 0; border-bottom-width: 10px; border-bottom-color: #000; border-bottom-style: solid; margin: 0; padding: 0;" align="center">
+						<!--[if (gte mso 9)|(IE)]>
+						<table cellpadding="0" cellspacing="0" width="100%">
+							<tr>
+								<td width="100%" valign="bottom">
+						<![endif]-->
+							<div class="column logo" style="vertical-align: bottom; margin: 0;">
+								<table cellpadding="0" cellspacing="0" width="100%" style="border-spacing: 0; margin: 0; padding: 0; font-family: Helvetica, Arial, Geneva, sans-serif; color: #1a1818; mso-table-lspace: 0pt; mso-table-rspace: 0pt; border-collapse: collapse;">
+									<tr>
+										<td class="inner" style="border-collapse: collapse; margin: 0; padding: 0; max-height: 90px; mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-size: 0; line-height: 0px;" valign="bottom">
+											<table cellpadding="0" cellspacing="0" class="contents" style="border-spacing: 0; margin: 0; padding: 0; font-family: Helvetica, Arial, Geneva, sans-serif; color: #1a1818; mso-table-lspace: 0pt; mso-table-rspace: 0pt; border-collapse: collapse; width: 100%; font-size: 16px; text-align: left;">
+												<tr>
+													<td style="border-collapse: collapse; margin: 0; padding: 0; max-height: 90px; mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-size: 0; line-height: 0px;" valign="bottom">
+														<a href="https://www.minnpost.com/" style="color: #801019; text-decoration: none;"><img src="https://www.minnpost.com/sites/default/themes/siteskin/inc/images/newsletter-logo-daily.png" alt="MinnPost" width="390" height="90" align="left" style="height: auto; margin: 0; padding: 0; border: 0 none; line-height: 100%; outline: none; text-decoration: none; width: 100%; max-width: 390px; max-height: 90px; display: block; vertical-align: bottom;" /></a>
+													</td>
+												</tr>
+											</table>
+										</td>
+									</tr>
+								</table>
+							</div>
+						<!--[if (gte mso 9)|(IE)]>
+								</td>
+							</tr>
+						</table>
+						<![endif]-->
+						</td> <!-- end .two-column.header -->
+					</tr> <!-- end row -->
+
+					<tr>
+						<td class="one-column content story" style="border-collapse: collapse; font-size: 0; margin: 0; padding: 0;" align="center">
+						<!--[if (gte mso 9)|(IE)]>
+						<table cellpadding="0" cellspacing="0" width="100%">
+							<tr>
+								<td width="100%" valign="middle">
+						<![endif]-->
+							<div class="column story story-first">
+									<div class="story-inner" style="border-bottom-width: 2px; border-bottom-color: #cccccf; border-bottom-style: solid; padding-bottom: 18px; margin-bottom: 18px; margin-top: 18px;">
+										<table cellpadding="0" cellspacing="0" class="bodyTable" style="border-spacing: 0; margin: 0; padding: 0; font-family: Helvetica, Arial, Geneva, sans-serif; color: #1a1818; mso-table-lspace: 0pt; mso-table-rspace: 0pt; border-collapse: collapse; text-align: left;">
+											<tr>
+												<td valign="top" style="border-collapse: collapse; margin: 0; padding: 0;">
+													<div>
+														
+														<p>put some content in here.</p>
+
+													</div>
+												</td>
+											</tr>
+										</table>
+									</div> <!-- end .story-inner -->
+								</div> <!-- end .story -->
+								<!--[if (gte mso 9)|(IE)]>
+									</td>
+								</tr>
+							</table>
+							<![endif]-->
+							</td> <!-- end .two-column.content.story -->
+						</tr> <!-- end row -->
+
+				</table> <!-- end .outer -->
+						<!--[if (gte mso 9)|(IE)]>
+						</td>
+					</tr>
+				</table>
+			<![endif]-->
+
+		</div>
+	</center>
+
+	</body>
+</html>
