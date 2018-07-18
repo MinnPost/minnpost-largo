80a8d0ea677c18e57ec0635de6139130ec03a160
Jonathan Stegall <jstegall@minnpost.com>
2018-07-18 16:02:56 -0500

    better error handling for homepage if zoninator is missing

--- front-page.php
+++ front-page.php
@@ -10,56 +10,50 @@
  */
 
 get_header(); ?>
-		
 	<div id="primary" class="m-layout-primary o-homepage-listing">
 		<main id="main" class="site-main m-homepage-zones">
-			<?php
-			if ( function_exists( 'z_get_zone' ) ) {
-				$the_query = z_get_zone_query( 'homepage-top' );
-			}
-			if ( $the_query->have_posts() ) : ?>
-				<?php $post_count = $the_query->post_count; ?>
-				<section class="m-zone m-zone-homepage-top m-archive m-archive-top m-archive-has-<?php echo $post_count; ?>-post">
-					<?php
-					while ( $the_query->have_posts() ) : $the_query->the_post();
-						get_template_part( 'template-parts/content', 'top' ); // content-top
-					endwhile;
-					?>
-				</section>
+			<?php if ( function_exists( 'z_get_zone' ) ) : ?>
+				<?php $the_query = z_get_zone_query( 'homepage-top' ); ?>
+				<?php if ( $the_query->have_posts() ) : ?>
+					<?php $post_count = $the_query->post_count; ?>
+					<section class="m-zone m-zone-homepage-top m-archive m-archive-top m-archive-has-<?php echo $post_count; ?>-post">
+						<?php
+						while ( $the_query->have_posts() ) : $the_query->the_post();
+							get_template_part( 'template-parts/content', 'top' ); // content-top
+						endwhile;
+						?>
+					</section>
+				<?php endif; ?>
 			<?php endif; ?>
-			
-			<?php
-			if ( function_exists( 'z_get_zone' ) ) {
-				$the_query = z_get_zone_query( 'homepage-middle' );
-			}
-			if ( $the_query->have_posts() ) : ?>
-				<section class="m-zone m-zone-homepage-middle m-archive m-archive-middle">
-					<?php
-					while ( $the_query->have_posts() ) : $the_query->the_post();
-						get_template_part( 'template-parts/content', 'middle' ); // content-middle
-					endwhile;
-					?>
-				</section>
+			<?php if ( function_exists( 'z_get_zone' ) ) : ?>
+				<?php $the_query = z_get_zone_query( 'homepage-middle' ); ?>
+				<?php if ( $the_query->have_posts() ) : ?>
+					<section class="m-zone m-zone-homepage-middle m-archive m-archive-middle">
+						<?php
+						while ( $the_query->have_posts() ) : $the_query->the_post();
+							get_template_part( 'template-parts/content', 'middle' ); // content-middle
+						endwhile;
+						?>
+					</section>
+				<?php endif; ?>
 			<?php endif; ?>
 
 			<div class="m-ad-region m-ad-region-home">
 				<?php do_action( 'acm_tag', 'Middle3' ); ?>
 			</div>
 
-			<?php
-			if ( function_exists( 'z_get_zone' ) ) {
-				$the_query = z_get_zone_query( 'homepage-bottom' );
-			}
-			if ( $the_query->have_posts() ) : ?>
-				<section class="m-zone m-zone-homepage-bottom m-archive m-archive-excerpt">
-					<?php
-					while ( $the_query->have_posts() ) : $the_query->the_post();
-						get_template_part( 'template-parts/content', 'excerpt' ); // content-excerpt
-					endwhile;
-					?>
-				</section>
+			<?php if ( function_exists( 'z_get_zone' ) ) : ?>
+				<?php $the_query = z_get_zone_query( 'homepage-bottom' ); ?>
+				<?php if ( $the_query->have_posts() ) : ?>
+					<section class="m-zone m-zone-homepage-bottom m-archive m-archive-excerpt">
+						<?php
+						while ( $the_query->have_posts() ) : $the_query->the_post();
+							get_template_part( 'template-parts/content', 'excerpt' ); // content-excerpt
+						endwhile;
+						?>
+					</section>
+				<?php endif; ?>
 			<?php endif; ?>
-
 		</main><!-- #main -->
 
 		<aside id="content-sidebar" class="o-content-sidebar" role="complementary">
