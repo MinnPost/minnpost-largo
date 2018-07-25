00656c3ddb02234b557a9e93a45c76ea93ce3a26
Jonathan Stegall <jstegall@minnpost.com>
2018-07-25 13:21:59 -0500

    specify some content types we do not want in the elasticsearch index

--- inc/custom-types.php
+++ inc/custom-types.php
@@ -44,3 +44,30 @@ if ( ! function_exists( 'create_newsletter' ) ) :
 		);
 	}
 endif;
+
+if ( ! function_exists( 'minnpost_indexable_post_types' ) ) :
+	add_filter( 'ep_indexable_post_types', 'minnpost_indexable_post_types' );
+	function minnpost_indexable_post_types( $post_types ) {
+		/* default value here is:
+		$post_types = array(
+			'post'           => 'post',
+			'page'           => 'page',
+			'attachment'     => 'attachment',
+			'partner'        => 'partner',
+			'partner_offer'  => 'partner_offer',
+			'popup'          => 'popup',
+			'guest'-author   => 'guest-author',
+			'cr3ativsponsor' => 'cr3ativsponsor',
+			'wp_log'         => 'wp_log',
+			'tribe_events'   => 'tribe_events',
+			'newsletter'     => 'newsletter',
+		);
+		*/
+		$non_indexable_types = array(
+			'partner_offer' => 'partner_offer',
+			'wp_log'        => 'wp_log',
+		);
+		$post_types          = array_diff_assoc( $post_types, $non_indexable_types );
+		return $post_types;
+	}
+endif;
