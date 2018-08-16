143da2e73afd086de5cb56e34652b1dfcb2a3979
Jonathan Stegall <jstegall@minnpost.com>
2018-08-15 23:16:46 -0500

    start a message template for header messages

---
+++ wp-message-inserter-plugin-templates/front-end/message-header.php
@@ -0,0 +1,4 @@
+<?php $message = $attributes['message']; ?>
+<div class="o-wrapper o-wrapper-message o-wrapper-message-<?php echo $message['ID']; ?>">
+	<?php echo $message['post_title']; ?>
+</div>
