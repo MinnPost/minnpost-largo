b3c661d07d97ab535861c40f6a230a8d2286b2bf
Jonathan Stegall <jstegall@minnpost.com>
2018-09-06 14:52:25 -0500

    template code standards

--- single-newsletter.php
+++ single-newsletter.php
@@ -8,8 +8,11 @@
  */
 
 get_header( 'newsletter' );
-	while ( have_posts() ) : the_post();
-		$newsletter_type = get_post_meta( get_the_ID(), '_mp_newsletter_type', true );
-		get_template_part( 'template-parts/content-newsletter', $newsletter_type );
-	endwhile; // End of the loop.
+
+while ( have_posts() ) :
+	the_post();
+	$newsletter_type = get_post_meta( get_the_ID(), '_mp_newsletter_type', true );
+	get_template_part( 'template-parts/content-newsletter', $newsletter_type );
+endwhile; // End of the loop.
+
 get_footer( 'newsletter' );
