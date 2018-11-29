566b60f977ebec3a6ebd96d46ff86a340db0025f
Jonathan Stegall <jstegall@minnpost.com>
2018-11-29 15:00:00 -0600

    it does make sense for all gravityforms to have honepot enabled

--- inc/custom-forms.php
+++ inc/custom-forms.php
@@ -165,3 +165,18 @@ endif;
 
 // remove Add Form gravityforms button from post forms
 add_filter( 'gform_display_add_form_button', '__return_false' );
+
+/**
+ * Enforce anti-spam honeypot on all Gravity forms.
+ *
+ * @param array $form The current form to be filtered.
+ * 
+ * @return array
+ */
+if ( ! function_exists( 'minnpost_largo_gf_honeypot' ) ) :
+	add_filter( 'gform_form_post_get_meta', 'minnpost_largo_gf_honeypot' );
+	function minnpost_largo_gf_honeypot( $form ) {
+		$form['enableHoneypot'] = true;
+		return $form;
+	}
+endif;
