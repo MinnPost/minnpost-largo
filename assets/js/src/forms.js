4dc2819834cb678906e060e4850e07fa77107531
Jonathan Stegall <jstegall@minnpost.com>
2019-02-08 14:29:38 -0600

    add new email addresses via ajax call to rest api

--- assets/js/src/forms.js
+++ assets/js/src/forms.js
@@ -1,109 +1,149 @@
-function gtag_report_conversion(url) {
-	var callback = function () {
-	  if (typeof(url) != 'undefined') {
-	    window.location = url;
-	  }
-	};
-	gtag('event', 'conversion', {
-	  'send_to': 'AW-976620175/jqCyCL7atXkQj5XY0QM',
-	  'event_callback': callback
-	});
-	return false;
-}
-
-function manageEmails() {
-	var form               = $('#account-settings-form');
-	var submitUrl          = form.attr( 'action' );
-	var nextEmailCount     = 1;
-	var emailToRemove      = '';
-	var consolidatedEmails = [];
-	/*if ( $('.m-user-email-list').length > 0 ) {
-		// if a user removes an email, take it away from the visual and from the form
-		$('.a-form-caption.a-remove-email').change( function( event ) {
-			emailToRemove = $(this).val();
-			$( this ).parents( 'li' ).remove();
+(function($){
+	function gtag_report_conversion(url) {
+		var callback = function () {
+		  if (typeof(url) != 'undefined') {
+		    window.location = url;
+		  }
+		};
+		gtag('event', 'conversion', {
+		  'send_to': 'AW-976620175/jqCyCL7atXkQj5XY0QM',
+		  'event_callback': callback
 		});
-		nextEmailCount = $('.m-user-email-list li').length;
-	}*/
-	// if a user wants to add an email, give them a properly numbered field
-	$('.a-form-caption.a-add-email').click( function( event ) {
-		event.preventDefault();
-		$('.a-form-caption.a-add-email').before('<input type="email" name="_consolidated_emails_array[' + nextEmailCount + ']" id="_consolidated_emails_array[' + nextEmailCount + ']" value="">')
-		nextEmailCount++;
-		
-	});
-}
-
-jQuery( document ).ready( function( $ ) {
-	"use strict";
-	if ( $('.m-form-email').length > 0 ) {
-		manageEmails();
+		return false;
 	}
-	if ( $('.m-form-newsletter-shortcode').length > 0 ) {
-		$('.m-form-newsletter-shortcode fieldset').before('<div class="m-hold-message"></div>');
-		$('.m-form-newsletter-shortcode form').submit(function(event) {
-			var that = this;
-			event.preventDefault(); // Prevent the default form submit.
-			var button = $('button', this);
-			button.prop('disabled', true);
-			button.text('Processing');
-			// serialize the form data
-			var ajax_form_data = $(this).serialize();
-			//add our own ajax check as X-Requested-With is not always reliable
-			ajax_form_data = ajax_form_data + '&ajaxrequest=true&subscribe';
+
+	function manageEmails() {
+		var form               = $('#account-settings-form');
+		var rest_root          = user_account_management_rest.site_url + user_account_management_rest.rest_namespace;
+		var full_url           = rest_root + '/' + 'update-user/';
+		var nextEmailCount     = 1;
+		var emailToRemove      = '';
+		var consolidatedEmails = [];
+		var newEmails          = [];
+		var ajax_form_data     = '';
+		if ( $('.m-user-email-list').length > 0 ) {
+			nextEmailCount = $('.m-user-email-list > li').length;
+			// if a user removes an email, take it away from the visual and from the form
+			$('.a-form-caption.a-remove-email').change( function( event ) {
+				//emailToRemove = $(this).val();
+				//$( this ).parents( 'li' ).remove();
+				ajax_form_data = $( this ).serialize(); //add our own ajax check as X-Requested-With is not always reliable
+
+				ajax_form_data = ajax_form_data + '&ajaxrequest=true&subscribe';
+				$.ajax({
+					url: full_url,
+					type: 'post',
+					dataType: 'json',
+					data: ajax_form_data
+				}).done(function (response) {
+					console.log('respond');
+				});
+
+			});
+			nextEmailCount = $('.m-user-email-list > li').length;
+		}
+		// if a user wants to add an email, give them a properly numbered field
+		$('.a-form-caption.a-add-email').click( function( event ) {
+			event.preventDefault();
+			$('.a-form-caption.a-add-email').before('<div class="a-input-with-button a-button-sentence"><input type="email" name="_consolidated_emails_array[]" id="_consolidated_emails_array[]" value=""><button type="submit" name="a-add-email-' + nextEmailCount + '" id="a-add-email-' + nextEmailCount + '" class="a-button">Add</button></div>');
+			nextEmailCount++;
+		});
+		$( form ).on( 'submit', function( event ) {
+			event.preventDefault();
+			ajax_form_data = form.serialize(); //add our own ajax check as X-Requested-With is not always reliable
+			ajax_form_data = ajax_form_data + '&rest=true';
 			$.ajax({
-				url: params.ajaxurl, // domain/wp-admin/admin-ajax.php
+				url: full_url,
 				type: 'post',
-				dataType : 'json',
+				beforeSend: function ( xhr ) {
+			        xhr.setRequestHeader( 'X-WP-Nonce', user_account_management_rest.nonce );
+			    },
+				dataType: 'json',
 				data: ajax_form_data
-			})
-			.done(function(response) { // response from the PHP action
-				var message = '';
-				if ( response.success === true ) {
-					$('fieldset', that).hide();
-					button.text('Thanks');
-					var analytics_action = 'Signup';
-					switch (response.data.user_status) {
-						case 'existing':
-							analytics_action = 'Update';
-							message = 'Thanks for updating your email preferences. They will go into effect immediately.';
-							break;
-						case 'new':
-							analytics_action = 'Signup';
-							message = 'We have added you to the MinnPost mailing list.';
-							break;
-						case 'pending':
-							analytics_action = 'Signup';
-							message = 'We have added you to the MinnPost mailing list. You will need to click the confirmation link in the email we sent to begin receiving messages.';
-							break;
+			}).done(function(data) {
+				newEmails = $( 'input[name="_consolidated_emails_array[]"]' ).map( function() {
+					return $(this).val();
+				}).get();
+				$.each( newEmails, function( index, value ) {
+					nextEmailCount = nextEmailCount + index;
+					$( '.m-user-email-list' ).append('<li>' + value + '<ul class="a-form-caption a-user-email-actions"><li class="a-form-caption a-make-primary-email"><input type="radio" name="primary_email" id="primary_email_' + nextEmailCount + '" value="' + value + '"><label for="primary_email_' + nextEmailCount + '"><small>Make Primary</small></label></li><li class="a-form-caption a-remove-email"><input type="checkbox" name="remove_email[' + nextEmailCount + ']" id="remove_email_' + nextEmailCount + '" value="' + value + '"><label for="remove_email_' + nextEmailCount + '"><small>Remove</small></label></li></ul></li>' );
+				});
+				$( '.m-form-change-email .a-input-with-button' ).remove();
+			});
+		});
+	}
+
+	jQuery( document ).ready( function( $ ) {
+		"use strict";
+		if ( $('.m-form-email').length > 0 ) {
+			manageEmails();
+		}
+		if ( $('.m-form-newsletter-shortcode').length > 0 ) {
+			$('.m-form-newsletter-shortcode fieldset').before('<div class="m-hold-message"></div>');
+			$('.m-form-newsletter-shortcode form').submit(function(event) {
+				var that = this;
+				event.preventDefault(); // Prevent the default form submit.
+				var button = $('button', this);
+				button.prop('disabled', true);
+				button.text('Processing');
+				// serialize the form data
+				var ajax_form_data = $(this).serialize();
+				//add our own ajax check as X-Requested-With is not always reliable
+				ajax_form_data = ajax_form_data + '&ajaxrequest=true&subscribe';
+				$.ajax({
+					url: params.ajaxurl, // domain/wp-admin/admin-ajax.php
+					type: 'post',
+					dataType : 'json',
+					data: ajax_form_data
+				})
+				.done(function(response) { // response from the PHP action
+					var message = '';
+					if ( response.success === true ) {
+						$('fieldset', that).hide();
+						button.text('Thanks');
+						var analytics_action = 'Signup';
+						switch (response.data.user_status) {
+							case 'existing':
+								analytics_action = 'Update';
+								message = 'Thanks for updating your email preferences. They will go into effect immediately.';
+								break;
+							case 'new':
+								analytics_action = 'Signup';
+								message = 'We have added you to the MinnPost mailing list.';
+								break;
+							case 'pending':
+								analytics_action = 'Signup';
+								message = 'We have added you to the MinnPost mailing list. You will need to click the confirmation link in the email we sent to begin receiving messages.';
+								break;
+						}
+						if ( response.data.confirm_message !== '' ) {
+							message = response.data.confirm_message;
+						}
+						if ( 'function' === typeof mp_analytics_tracking_event ) {
+							mp_analytics_tracking_event( 'event', 'Newsletter', analytics_action, location.pathname );
+							gtag_report_conversion( location.pathname );
+						}
+					} else {
+						button.prop('disabled', false);
+						button.text('Subscribe');
+						if ( 'function' === typeof mp_analytics_tracking_event ) {
+							mp_analytics_tracking_event( 'event', 'Newsletter', 'Fail', location.pathname );
+						}
 					}
-					if ( response.data.confirm_message !== '' ) {
-						message = response.data.confirm_message;
-					}
-					if ( 'function' === typeof mp_analytics_tracking_event ) {
-						mp_analytics_tracking_event( 'event', 'Newsletter', analytics_action, location.pathname );
-						gtag_report_conversion( location.pathname );
-					}
-				} else {
+					$('.m-hold-message').html('<div class="m-form-message m-form-message-info">' + message + '</div>');
+				})
+				.fail(function(response) {
+					$('.m-hold-message').html('<div class="m-form-message m-form-message-info">An error has occured. Please try again.</div>');
 					button.prop('disabled', false);
 					button.text('Subscribe');
 					if ( 'function' === typeof mp_analytics_tracking_event ) {
 						mp_analytics_tracking_event( 'event', 'Newsletter', 'Fail', location.pathname );
 					}
-				}
-				$('.m-hold-message').html('<div class="m-form-message m-form-message-info">' + message + '</div>');
-			})
-			.fail(function(response) {
-				$('.m-hold-message').html('<div class="m-form-message m-form-message-info">An error has occured. Please try again.</div>');
-				button.prop('disabled', false);
-				button.text('Subscribe');
-				if ( 'function' === typeof mp_analytics_tracking_event ) {
-					mp_analytics_tracking_event( 'event', 'Newsletter', 'Fail', location.pathname );
-				}
-			})
-			.always(function() {
-				event.target.reset();
+				})
+				.always(function() {
+					event.target.reset();
+				});
 			});
-		});
-	}
-});
+		}
+	});
+})(jQuery);
