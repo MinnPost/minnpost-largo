e786ee579fa6466801a806339ad46a1782ae4e85
Jonathan Stegall <jstegall@minnpost.com>
2019-02-12 12:25:50 -0600

    make the script less likely to miss emails that are being removed. also account for users who have only one email address and then add another one

--- assets/js/src/forms.js
+++ assets/js/src/forms.js
@@ -40,6 +40,7 @@
 		// start out with no primary/removals checked
 		$( '.a-form-caption.a-make-primary-email input[type="radio"]' ).prop( 'checked', false );
 		$( '.a-form-caption.a-remove-email input[type="checkbox"]' ).prop( 'checked', false );
+		// if there is a list of emails (not just a single form field)
 		if ( $( '.m-user-email-list' ).length > 0 ) {
 			nextEmailCount = $( '.m-user-email-list > li' ).length;
 			// if a user selects a new primary, move it into that position
@@ -91,19 +92,23 @@
 				$( '.m-user-email-list > li' ).each( function( index ) {
 					if ( $( this ).contents().get( 0 ).nodeValue !== emailToRemove ) {
 						consolidatedEmails.push( $( this ).contents().get( 0 ).nodeValue );
+					} else {
+						consolidatedEmails.splice( $.inArray( consolidatedEmails, $( this ).contents().get( 0 ).nodeValue ), 1 );
 					}
 				});
-				// get or don't get confirmation from user
+				// get or don't get confirmation from user for removal
 				that = $( this ).parent().parent();
 				$( '.a-pre-confirm', that ).hide();
 				$( '.a-form-confirm', that ).show();
 				$( this ).parent().parent().addClass( 'a-pre-confirm' );
 				$( this ).parent().parent().removeClass( 'a-stop-confirm' );
-				//$( this ).parent().after( confirmChange );
 				$( this ).parent().parent().append( confirmChange );
+				// if confirmed, remove the email and submit the form
 				$( '.m-user-email-list' ).on( 'click', '#a-confirm-removal', function( event ) {
 					event.preventDefault();
-					$( this ).parents( 'li' ).remove();
+					$( this ).parents( 'li' ).fadeOut( 'normal', function() {
+						$( this ).remove();
+					});
 					$( '#_consolidated_emails' ).val( consolidatedEmails.join( ',' ) );
 					nextEmailCount = $( '.m-user-email-list > li' ).length;
 					form.submit();
@@ -118,13 +123,13 @@
 		}
 
 		// if a user wants to add an email, give them a properly numbered field
-		$('.a-form-caption.a-add-email').on( 'click', function( event ) {
+		$( '.m-form-email' ).on( 'click', '.a-form-caption.a-add-email', function( event ) {
 			event.preventDefault();
 			$('.a-form-caption.a-add-email').before( '<div class="a-input-with-button a-button-sentence"><input type="email" name="_consolidated_emails_array[]" id="_consolidated_emails_array[]" value=""><button type="submit" name="a-add-email-' + nextEmailCount + '" id="a-add-email-' + nextEmailCount + '" class="a-button a-button-add-user-email">Add</button></div>' );
 			nextEmailCount++;
 		});
 
-		$( form ).on( 'submit', function( event ) {
+		$( '.m-entry-content' ).on( 'submit', form, function( event ) {
 			event.preventDefault();
 			ajax_form_data = form.serialize(); //add our own ajax check as X-Requested-With is not always reliable
 			ajax_form_data = ajax_form_data + '&rest=true';
@@ -142,10 +147,16 @@
 				}).get();
 				$.each( newEmails, function( index, value ) {
 					nextEmailCount = nextEmailCount + index;
-					$( '.m-user-email-list' ).append( '<li>' + value + '<ul class="a-form-caption a-user-email-actions"><li class="a-form-caption a-pre-confirm a-make-primary-email"><input type="radio" name="primary_email" id="primary_email_' + nextEmailCount + '" value="' + value + '"><label for="primary_email_' + nextEmailCount + '"><small>Make Primary</small></label></li><li class="a-form-caption a-pre-confirm a-remove-email"><input type="checkbox" name="remove_email[' + nextEmailCount + ']" id="remove_email_' + nextEmailCount + '" value="' + value + '"><label for="remove_email_' + nextEmailCount + '"><small>Remove</small></label></li></ul></li>' );
+					$( '.m-user-email-list' ).append( '<li id="user-email-' + nextEmailCount + '">' + value + '<ul class="a-form-caption a-user-email-actions"><li class="a-form-caption a-pre-confirm a-make-primary-email"><input type="radio" name="primary_email" id="primary_email_' + nextEmailCount + '" value="' + value + '"><label for="primary_email_' + nextEmailCount + '"><small>Make Primary</small></label></li><li class="a-form-caption a-pre-confirm a-remove-email"><input type="checkbox" name="remove_email[' + nextEmailCount + ']" id="remove_email_' + nextEmailCount + '" value="' + value + '"><label for="remove_email_' + nextEmailCount + '"><small>Remove</small></label></li></ul></li>' );
 					$( '#_consolidated_emails' ).val( $( '#_consolidated_emails' ).val() + ',' + value );
 				});
 				$( '.m-form-change-email .a-input-with-button' ).remove();
+				if ( $( '.m-user-email-list' ).length === 0 ) {
+					if ( $( 'input[name="_consolidated_emails_array[]"]' ) !== $( 'input[name="email"]' ) ) {
+						// it would be nice to only load the form, but then click events still don't work
+						location.reload();
+					}
+				}
 			});
 		});
 	}
