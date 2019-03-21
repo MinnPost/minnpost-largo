5fe9be9bc0e751380039e28a23dddca6bf139906
Jonathan Stegall <jstegall@minnpost.com>
2019-03-21 10:51:46 -0500

    disallow non production sites on robots.txt

--- functions.php
+++ functions.php
@@ -125,6 +125,11 @@ require get_theme_file_path() . '/inc/custom-scripts-and-styles.php';
 require get_theme_file_path() . '/inc/pwa.php';
 
 /**
+ * Settings for robots.txt
+ */
+require get_theme_file_path() . '/inc/robots.php';
+
+/**
  * Jetpack related methods
  */
 require get_theme_file_path() . '/inc/jetpack.php';
---
+++ inc/robots.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Robots.txt methods
+ * Outputs at /robots.txt
+ *
+ * @package MinnPost Largo
+ */
+
+/**
+* Nofollow for non-production domains
+*
+* @param array $args
+* @return array $args
+*
+*/
+if ( ! function_exists( 'minnpost_largo_domain_nofollow' ) ) :
+	add_action( 'do_robotstxt', 'minnpost_largo_domain_nofollow' );
+	function minnpost_largo_domain_nofollow() {
+		if ( 'production' !== VIP_GO_ENV ) {
+			echo 'User-agent: *' . PHP_EOL;
+			echo 'Disallow: /' . PHP_EOL;
+		}
+	}
+endif;
