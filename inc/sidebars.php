4fe88b8eb696300dfdcd61349b729828441216cd
Jonathan Stegall <jstegall@minnpost.com>
2019-07-28 20:59:51 -0500

    add a separate sidebar only for the glean, and insert it into the homepage content flow. this lets it vary based on whether there's a lead story or not.

--- inc/sidebars.php
+++ inc/sidebars.php
@@ -72,5 +72,16 @@ if ( ! function_exists( 'minnpost_largo_widgets_init' ) ) :
 				'after_title'   => '</h3>',
 			)
 		);
+		register_sidebar(
+			array(
+				'name'          => __( 'Homepage Glean', 'minnpost_largo' ),
+				'id'            => 'sidebar-4',
+				'description'   => __( 'This should only contain the Glean that sits within the homepage content flow.', 'minnpost_largo' ),
+				'before_widget' => '<section id="%1$s" class="m-widget-glean m-widget-glean-home-content %2$s">',
+				'after_widget'  => '</section>',
+				'before_title'  => '<h3 class="a-widget-title">',
+				'after_title'   => '</h3>',
+			)
+		);
 	}
 endif;
