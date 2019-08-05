c256552439c15909958c0091f4c1234542b1f917
Jonathan Stegall <jstegall@minnpost.com>
2019-08-05 11:34:45 -0500

    add unpublished indicator to admin menu bar

--- inc/menus.php
+++ inc/menus.php
@@ -607,3 +607,64 @@ if ( function_exists( 'wp_cache_flush' ) ) :
 		$wp_admin_bar->add_node( $args );
 	}
 endif;
+
+/**
+* Add unpublished indicator to admin bar menu
+*
+*/
+if ( ! function_exists( 'minnpost_largo_unpublished_indicator' ) ) :
+	add_action( 'admin_bar_menu', 'minnpost_largo_unpublished_indicator', 81 );
+	function minnpost_largo_unpublished_indicator( $wp_admin_bar ) {
+
+		if ( ! current_user_can( 'edit_posts' ) || ! is_singular() ) {
+			return;
+		}
+
+		$user = wp_get_current_user();
+		// not for comment moderators
+		if ( in_array( 'comment_moderator', (array) $user->roles ) ) {
+			return;
+		}
+
+		$post_id = get_the_ID();
+
+		if ( false === $post_id ) {
+			return;
+		}
+
+		$post_status = get_post_status( $post_id );
+
+		if ( 'publish' === $post_status ) {
+			return;
+		}
+
+		$args = array(
+			'id'    => 'item_unpublishd',
+			'title' => __( 'Unpublished', 'minnpost-largo' ),
+			//'href'  => wp_nonce_url( $dashboard_url, 'flush-cache-button' ),
+			'meta'  => array(
+				'class' => 'unpublished-indicator',
+			),
+		);
+		$wp_admin_bar->add_node( $args );
+	}
+endif;
+
+/**
+* Any admin bar CSS customizations should go here so they're consistent on front and back end
+*
+*/
+if ( ! function_exists( 'minnpost_style_tool_bar' ) ) :
+	add_action( 'admin_head', 'minnpost_style_tool_bar' );
+	add_action( 'wp_head', 'minnpost_style_tool_bar' );
+	function minnpost_style_tool_bar() {
+		echo '
+		<style>
+		#wp-admin-bar-item_unpublishd div, #wpadminbar:not(.mobile) #wp-admin-bar-item_unpublishd:hover div {
+			text-transform: uppercase;
+			background: #b60;
+			color: #fff;
+		}
+	    </style>';
+	}
+endif;
