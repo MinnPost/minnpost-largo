675aa575d1686ec8304e77fb6fe40f55854f0503
Jonathan Stegall <jstegall@minnpost.com>
2019-09-19 11:42:37 -0500

    for #61, allow any post status on these search fields

--- inc/custom-fields.php
+++ inc/custom-fields.php
@@ -98,7 +98,7 @@ if ( function_exists( 'create_newsletter' ) ) :
 			'orderby'        => 'modified',
 			'order'          => 'DESC',
 			'post_type'      => $object_type,
-			'post_status'    => 'publish',
+			'post_status'    => 'any',
 		);
 		$most_recent_newsletter = wp_get_recent_posts( $recent_newsletter_args, OBJECT );
 		if ( is_object( $most_recent_newsletter[0] ) ) {
@@ -111,6 +111,7 @@ if ( function_exists( 'create_newsletter' ) ) :
 			'post_type'      => array( 'post' ),
 			'orderby'        => 'modified',
 			'order'          => 'DESC',
+			'post_status'    => 'any',
 			'date_query'     => array(
 				array(
 					'column' => 'post_modified',
@@ -138,8 +139,9 @@ if ( function_exists( 'create_newsletter' ) ) :
 					'desc'       => __( 'Search for a post here', 'minnpost-largo' ),
 					'id'         => $prefix . 'top_posts',
 					'query_args' => array(
-						'orderby' => 'modified',
-						'order'   => 'DESC',
+						'orderby'     => 'modified',
+						'order'       => 'DESC',
+						'post_status' => 'any',
 					),
 				),
 				'post_search_ajax'
@@ -170,8 +172,9 @@ if ( function_exists( 'create_newsletter' ) ) :
 					'desc'       => __( 'Search for a post here', 'minnpost-largo' ),
 					'id'         => $prefix . 'more_posts',
 					'query_args' => array(
-						'orderby' => 'modified',
-						'order'   => 'DESC',
+						'orderby'     => 'modified',
+						'order'       => 'DESC',
+						'post_status' => 'any',
 					),
 				)
 			)
