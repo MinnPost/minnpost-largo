24e1f2589366d5a58513ad69475601d55a3959ab
Jonathan Stegall <jstegall@minnpost.com>
2020-03-17 16:16:07 -0500

    template for daily coronavirus update

---
+++ template-parts/content-newsletter-daily_coronavirus.php
@@ -0,0 +1,242 @@
+<?php
+/**
+ * Template part for displaying Daily Coronavirus content
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package MinnPost Largo
+ */
+
+?>
+<style type="text/css">
+.webkit {
+	max-width: 600px;
+}
+.ExternalClass {
+	width: 100%;
+}
+.ExternalClass, .ExternalClass p, .ExternalClass span, .ExternalClass font, .ExternalClass td, .ExternalClass div {
+	line-height: 100%;
+}
+
+a[x-apple-data-detectors] {
+	color: #801019 !important;
+	text-decoration: none !important;
+	font-size: inherit !important;
+	font-family: inherit !important;
+	font-weight: normal !important;
+	line-height: inherit !important;
+}
+</style>
+
+<!--[if (gte mso 9)|(IE)]>
+<style type="text/css">
+	.header table { font-size:1px; line-height:0; mso-Margin-top-alt:1px; }
+</style>
+<![endif]-->
+
+<style>
+	body {
+		Margin: 0; padding: 0; min-width: 100%; background-color: #ffffff;
+	}
+	img {
+		border: 0 none; height: auto; line-height: 100%; outline: none; text-decoration: none;
+	}
+	a:hover {
+		color: #801019 !important; text-decoration: underline !important;
+	}
+	a:active {
+		color: #801019 !important; text-decoration: underline !important;
+	}
+	a:visited {
+		color: #801019 !important;
+	}
+	h1 a:active {
+		color: #1A1818;
+	}
+	h2 a:active {
+		color: #1A1818;
+	}
+	h3 a:active {
+		color: #1A1818;
+	}
+	h4 a:active {
+		color: #1A1818;
+	}
+	h5 a:active {
+		color: #1A1818;
+	}
+	h6 a:active {
+		color: #1A1818;
+	}
+	h1 a:visited {
+		color: #1A1818;
+	}
+	h2 a:visited {
+		color: #1A1818;
+	}
+	h3 a:visited {
+		color: #1A1818;
+	}
+	h4 a:visited {
+		color: #1A1818;
+	}
+	h5 a:visited {
+		color: #1A1818;
+	}
+	h6 a:visited {
+		color: #1A1818;
+	}
+	.content .button td td a:hover {
+		color: #ffffff !important; text-decoration: none !important;
+	}
+	.content .button td td a:visited {
+		color: #ffffff !important; text-decoration: none !important;
+	}
+	.content .button td td a:active {
+		color: #ffffff !important; text-decoration: none !important;
+	}
+	@media only screen and (max-width: 600px) {
+		.two-column.header .date {
+			text-align: left !important;
+		}
+		.two-column.header .date td {
+			text-align: left !important;
+		}
+		.two-column .column {
+			max-width: 90% !important; Margin-right: auto !important; Margin-left: auto !important;
+		}
+		.two-column .column.ad {
+			max-width: 90% !important; Margin-right: auto !important; Margin-left: auto !important;
+		}
+		.two-column .column.more {
+			max-width: 90% !important; Margin-right: auto !important; Margin-left: auto !important;
+		}
+		.two-column .column.date {
+			max-width: 90% !important; Margin-right: auto !important; Margin-left: auto !important;
+		}
+		.two-column .column.logo {
+			max-width: 90% !important; Margin-right: auto !important; Margin-left: auto !important;
+		}
+		.one-column .column {
+			max-width: 90% !important; Margin-right: auto !important; Margin-left: auto !important;
+		}
+		.one-column .column.ad {
+			max-width: 90% !important; Margin-right: auto !important; Margin-left: auto !important;
+		}
+		.one-column .column.more {
+			max-width: 90% !important; Margin-right: auto !important; Margin-left: auto !important;
+		}
+		.one-column .column.date {
+			max-width: 90% !important; Margin-right: auto !important; Margin-left: auto !important;
+		}
+		.one-column .column.logo {
+			max-width: 90% !important; Margin-right: auto !important; Margin-left: auto !important;
+		}
+	}
+</style>
+
+<center class="wrapper" style="-ms-text-size-adjust: 100%; -webkit-text-size-adjust: 100%; table-layout: fixed; width: 100%">
+	<div class="webkit">
+		<!--[if (gte mso 9)|(IE)]>
+			<table cellpadding="0" cellspacing="0" width="600" align="center">
+				<tr>
+					<td>
+		<![endif]-->
+		<table cellpadding="0" cellspacing="0" class="outer" align="center" style="border-collapse: collapse; border-spacing: 0; color: #1a1818; font-family: Helvetica, Arial, Geneva, sans-serif; Margin: 0 auto; max-width: 600px; mso-table-lspace: 0pt; mso-table-rspace: 0pt; padding: 0; width: 100%; background-color: #ffffff;">
+			<tr>
+				<td class="one-column header" style="border-collapse: collapse; border-bottom-width: 2px; border-bottom-color: #cccccf; border-bottom-style: solid; Margin: 0; padding: 0;">
+				<!--[if (gte mso 9)|(IE)]>
+					<table cellpadding="0" cellspacing="0" width="100%">
+						<tr>
+							<td width="100%" valign="bottom">
+				<![endif]-->
+					<div class="column logo" style="width: 100%;">
+						<table cellpadding="0" cellspacing="0" width="100%" style="border-spacing: 0; Margin: 0; padding: 0; font-family: Helvetica, Arial, Geneva, sans-serif; color: #1A1818; mso-table-lspace: 0pt; mso-table-rspace: 0pt; border-collapse: collapse;">
+							<tr>
+								<td class="inner" style="border-collapse: collapse; Margin: 0; padding: 0; max-height: 125px; mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-size: 0; line-height: 0px;" valign="bottom">
+									<table cellpadding="0" cellspacing="0" class="contents" style="border-spacing: 0; Margin: 0; padding: 0; font-family: Helvetica, Arial, Geneva, sans-serif; color: #1A1818; mso-table-lspace: 0pt; mso-table-rspace: 0pt; border-collapse: collapse; width: 100%;">
+										<tr>
+											<td align="center" style="border-collapse: collapse; Margin: 0; padding: 0; max-height: 68px; mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-size: 0; line-height: 0px;" valign="bottom">
+												<img src="<?php minnpost_newsletter_logo( get_the_ID() ); ?>" alt="<?php bloginfo( 'name' ); ?>" width="600" height="125" style="border: 0 none; display: block; height: auto; line-height: 100%; Margin: 0; max-height: 125px; max-width: 600px; outline: none; padding: 0; text-decoration: none; vertical-align: bottom; width: 100%" />
+											</td>
+										</tr>
+									</table>
+								</td>
+							</tr>
+						</table>
+					</div>
+					<!--[if (gte mso 9)|(IE)]>
+							</td>
+						</tr>
+					</table>
+					<![endif]-->
+				</td> <!-- end .one-column.header -->
+			</tr> <!-- end row -->
+
+			<?php
+			$body = apply_filters( 'the_content', get_the_content() );
+			if ( '' !== $body ) :
+				$body = str_replace( '<a href="', '<a style="color: #801019; text-decoration: none;" href="', $body );
+				$body = str_replace( ' dir="ltr"', '', $body );
+				$body = str_replace( '<p class="intro">', '<p>', $body );
+				$body = preg_replace( '/<p>/', '<p class="intro" style="font-family: Georgia, \'Times New Roman\', Times, serif; font-size: 17.6px; line-height: 24.9444px; margin: 0 0 15px; padding: 15px 0 0;">', $body, 1 );
+				$body = str_replace( '<p>', '<p style="font-family: Georgia, \'Times New Roman\', Times, serif; font-size: 16px; line-height: 20.787px; margin: 0 0 15px; padding: 0;">', $body );
+				$body = str_replace( '<li>', '<li style="font-family: Georgia, \'Times New Roman\', Times, serif; font-size: 16px; line-height: 20.787px; margin: 0 0 15px; padding: 0;">', $body );
+				$body = str_replace( '<ul>', '<ul style="font-family: Georgia, \'Times New Roman\', Times, serif; font-size: 16px; line-height: 20.787px; margin: 0 0 15px; padding: 0 0 0 40px;">', $body );
+
+				// replace headings
+				$body = str_replace( '<h2>', '<h2 style="color: #801019; margin: 15px 0; display: block; font-size: 14px; line-height: 1; font-family: Helvetica, Arial, Geneva, sans-serif; font-weight: bold; text-transform: uppercase; border-top-width: 2px; border-top-color: #cccccf; border-top-style: solid; padding-top: 15px;">', $body );
+				$body = str_replace( '<h3>', '<h3 style="color: #801019; margin: 15px 0; display: block; font-size: 14px; line-height: 1; font-family: Helvetica, Arial, Geneva, sans-serif; font-weight: bold; text-transform: uppercase; border-top-width: 2px; border-top-color: #cccccf; border-top-style: solid; padding-top: 15px;">', $body );
+				$body = str_replace( '<h4>', '<h4 style="color: #801019; margin: 15px 0; display: block; font-size: 14px; line-height: 1; font-family: Helvetica, Arial, Geneva, sans-serif; font-weight: bold; text-transform: uppercase; border-top-width: 2px; border-top-color: #cccccf; border-top-style: solid; padding-top: 15px;">', $body );
+				$body = str_replace( '<blockquote><p style="font-family: Georgia, \'Times New Roman\', Times, serif; font-size: 16px; line-height: 20.787px; margin: 0 0 15px; padding: 0;">', '<blockquote style="border-left-width: 2px; border-left-color: #cccccf; border-left-style: solid; margin: 10px 10px 15px; padding: 0 10px; color: #6a6161;"><p style="font-family: Georgia, \'Times New Roman\', Times, serif; font-size: 16px; line-height: 20.787px; margin: 0 0 15px; padding: 0;">', $body );
+				?>
+				<tr>
+					<td class="one-column content story" style="border-collapse: collapse; Margin: 0; padding: 0;">
+					<!--[if (gte mso 9)|(IE)]>
+						<table cellpadding="0" cellspacing="0" width="100%">
+							<tr>
+								<td width="100%" valign="bottom">
+					<![endif]-->
+						<div class="column story story-first">
+							<div class="story-inner" style="Margin: 0;">
+								<table cellpadding="0" cellspacing="0" class="bodyTable forwardTable" style="border-spacing: 0; Margin: 0; padding: 0; font-family: Helvetica, Arial, Geneva, sans-serif; color: #1A1818; mso-table-lspace: 0pt; mso-table-rspace: 0pt; border-collapse: collapse; text-align: left;">
+									<tr>
+										<td valign="top" style="border-collapse: collapse; Margin: 0; padding: 0;">
+											<div>
+												<?php echo $body; ?>
+											</div>
+										</td>
+									</tr>
+								</table>
+							</div>
+						</div>
+						<!--[if (gte mso 9)|(IE)]>
+								</td>
+							</tr>
+						</table>
+						<![endif]-->
+					</td> <!-- end .one-column.story -->
+				</tr> <!-- end row -->
+			<?php endif; ?>
+
+			<tr>
+				<td class="one-column footer" style="border-collapse: collapse; Margin: 0; padding: 0">
+					<table cellpadding="0" cellspacing="0" width="100%" style="border-collapse: collapse; border-spacing: 0; color: #1a1818; font-family: Helvetica, Arial, Geneva, sans-serif; Margin: 0; mso-table-lspace: 0pt; mso-table-rspace: 0pt; padding: 0">
+						<tr>
+							<td class="inner contents" style="border-collapse: collapse; Margin: 0; padding: 0; width: 100%">
+								<?php dynamic_sidebar( 'sidebar-3' ); ?>
+								<?php
+								$footer_message = get_option( 'site_footer_message', '' );
+								if ( '' !== $footer_message ) :
+									?>
+									<p class="address" style="font-size: 12px; Margin: 10px 0 15px; padding: 0; text-align: center;" align="center"><?php echo $footer_message; ?></p>
+								<?php endif; ?>
+							</td>
+						</tr>
+					</table>
+				</td> <!-- end .one-column.footer -->
+			</tr> <!-- end row -->
+
+	</div>
+</center>
