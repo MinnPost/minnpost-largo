962dbaa88ab6552aed1fade35a27c100ae84e0ab
Jonathan Stegall <jstegall@minnpost.com>
2020-06-18 12:22:13 -0500

    responsive styles for event list

--- tribe/events/v2/list/event.php
+++ tribe/events/v2/list/event.php
@@ -22,12 +22,10 @@ $event_classes = tribe_get_post_class( array( 'm-post, m-event, tribe-events-cal
 	?>
 
 	<div class="m-event-details tribe-events-calendar-list__event-details tribe-common-g-col">
-
 		<header class="m-event-header m-entry-header tribe-events-calendar-list__event-header">
 			<?php $this->template( 'list/event/title', array( 'event' => $event ) ); ?>
 			<?php $this->template( 'list/event/venue', array( 'event' => $event ) ); ?>
 		</header>
-
 		<?php $this->template( 'list/event/date', array( 'event' => $event ) ); ?>
 		<?php $this->template( 'list/event/cost', array( 'event' => $event ) ); ?>
 	</div>
