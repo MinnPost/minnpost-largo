80de63b7a0604ddd0f6a400c6d0ed5bcfb678aca
Jonathan Stegall <jstegall@minnpost.com>
2020-06-18 13:16:07 -0500

    cleanup event template

--- tribe/events/v2/list/event.php
+++ tribe/events/v2/list/event.php
@@ -10,17 +10,6 @@ $event_classes = tribe_get_post_class( array( 'm-post, m-event, tribe-events-cal
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php tribe_classes( $event_classes ); ?>>
-
-	<?php
-	/*if ( ! isset( $show_image ) || true === $show_image ) {
-		minnpost_post_image(
-			'feature-medium',
-			array(),
-			$event->ID
-		);
-	}*/
-	?>
-
 	<div class="m-event-details tribe-events-calendar-list__event-details tribe-common-g-col">
 		<header class="m-event-header m-entry-header tribe-events-calendar-list__event-header">
 			<?php $this->template( 'list/event/title', array( 'event' => $event ) ); ?>
@@ -29,10 +18,9 @@ $event_classes = tribe_get_post_class( array( 'm-post, m-event, tribe-events-cal
 		<?php $this->template( 'list/event/date', array( 'event' => $event ) ); ?>
 		<?php $this->template( 'list/event/cost', array( 'event' => $event ) ); ?>
 	</div>
-
 	<?php if ( '' !== get_the_excerpt() ) : ?>
 		<div class="m-entry-excerpt m-entry-excerpt-event">
 			<?php the_excerpt(); ?>
-		</div><!-- .m-entry-excerpt -->
+		</div>
 	<?php endif; ?>
 </article>
