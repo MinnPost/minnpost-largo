c41b63ab467ad465d45940e45686b2d7a8e29dee
Jonathan Stegall <jstegall@minnpost.com>
2020-06-19 17:03:17 -0500

    allow title to be removed

--- template-parts/content-event.php
+++ template-parts/content-event.php
@@ -1,6 +1,15 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'm-post m-event m-event m-event-single' ); ?>>
 	<header class="m-event-header m-entry-header tribe-events-calendar-list__event-header">
-		<h1 class="a-entry-title a-event-title tribe-events-single-event-title"><?php echo get_the_title(); ?></h1>
+		<?php
+		$hide_title = get_post_meta( $id, '_mp_remove_title_from_display', true );
+		if ( 'on' !== $hide_title ) {
+			if ( is_single() ) :
+				the_title( '<h1 class="a-entry-title a-event-title tribe-events-single-event-title">', '</h1>' );
+			else :
+				the_title( '<h3 class="a-entry-title a-event-title tribe-events-single-event-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' );
+			endif;
+		}
+		?>
 		<div class="m-event-notices">
 			<?php tribe_the_notices(); ?>
 		</div>
