111fc6345658a03b883fb8aafb4e0ab98f02236c
Jonathan Stegall <jstegall@minnpost.com>
2020-07-01 13:27:50 -0500

    basic archive post styles

--- template-parts/content-excerpt.php
+++ template-parts/content-excerpt.php
@@ -10,20 +10,12 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'm-post' ); ?>>
-
 	<?php minnpost_post_image( 'feature-large' ); ?>
-
-	<?php if ( '' !== minnpost_get_category_name() || '' !== minnpost_get_replace_category_text() ) : ?>
-		<div class="m-post-classification">
-			<?php if ( '' === minnpost_get_replace_category_text() ) : ?>
-				<?php minnpost_category_breadcrumb(); ?>
-			<?php else : ?>
-				<?php minnpost_replace_category_text(); ?>
-			<?php endif; ?>
-			<?php minnpost_plus_icon(); ?>
-		</div>
+	<?php if ( '' === minnpost_get_replace_category_text() ) : ?>
+		<?php minnpost_category_breadcrumb(); ?>
+	<?php else : ?>
+		<?php minnpost_replace_category_text(); ?>
 	<?php endif; ?>
-
 	<header class="m-entry-header">
 		<?php the_title( '<h3 class="a-entry-title a-entry-title-excerpt"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' ); ?>
 		<?php if ( 'post' === get_post_type( $post->id ) ) : ?>
@@ -39,10 +31,7 @@
 			</div><!-- .m-entry-meta -->
 		<?php endif; ?>
 	</header><!-- .m-entry-header -->
-
 	<div class="m-entry-excerpt">
 		<?php the_excerpt(); ?>
 	</div><!-- .m-entry-excerpt -->
-
-
 </article><!-- #post-## -->
