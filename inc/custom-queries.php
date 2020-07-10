fc45bb3a9efa0c8cebe66ae766c57d9419cb0c67
Jonathan Stegall <jstegall@minnpost.com>
2020-07-10 12:08:44 -0500

    fix the query that runs the category archives so the correct categories show on parents, and the ineligible parents do not show on children

--- inc/custom-queries.php
+++ inc/custom-queries.php
@@ -34,27 +34,68 @@ endif;
 if ( ! function_exists( 'custom_archive_query_vars' ) ) :
 	add_action( 'pre_get_posts', 'custom_archive_query_vars' );
 	function custom_archive_query_vars( $query ) {
+		// only do stuff on archive templates when it's the main query
 		if ( $query->is_archive() && ! is_admin() && $query->is_main_query() ) {
+			// for category archives
 			if ( is_category() ) {
 				$category_id = $query->get_queried_object_id();
 				if ( function_exists( 'minnpost_get_grouped_categories' ) ) {
+
+					// term meta field where a category stores its parent
+					$grouping_field = '_mp_category_group';
+					// get the children of a parent category
 					$grouped_categories = minnpost_get_grouped_categories( $category_id );
-					if ( empty( $grouped_categories ) ) {
-						return;
+					// get the parent id of a child category
+					$grouping_category_id = get_term_meta( $category_id, $grouping_field, true );
+
+					// set up an array for changing the query
+					$taxonomy_parameters = array();
+
+					// if this category has a parent category id
+					// this means it's a child category like metro
+					if ( '' !== $grouping_category_id ) {
+						// get the children of the other parent categories that aren't this one
+						// this is an array of IDs to exclude
+						$exclude_ids = minnpost_get_grouping_categories_to_exclude( $grouping_category_id );
 					}
-					$tax_query = $query->get( 'tax_query' );
-					if ( ! is_array( $tax_query ) ) {
-						$tax_query = array();
+
+					// if this category has child categories
+					// this means it's a parent, like news
+					if ( ! empty( $grouped_categories ) ) {
+						// get the children of the other parent categories that aren't this one
+						// this is an array of IDs to exclude
+						$exclude_ids    = minnpost_get_grouping_categories_to_exclude( $category_id );
+						$category_ids   = $grouped_categories;
+						$category_ids[] = $category_id; // in case posts are added to the parent
+
+						// if this is a parent category, the query should not be limited to itself
+						$query->set( 'category_name', false );
+
+						// these are the category IDs of the childrten to include in the query
+						$taxonomy_parameters[] = array(
+							'taxonomy' => 'category',
+							'field'    => 'term_id',
+							'terms'    => $category_ids,
+						);
 					}
-					$taxquery[] = array(
-						'taxonomy' => 'category',
-						'field'    => 'term_id',
-						'terms'    => $grouped_categories,
-					);
-					$query->set( 'tax_query', $taxquery );
-					$query->set( 'category_name', false );
+
+					// if we have category IDs to exclude, exclude them from the query
+					if ( ! empty( $exclude_ids ) ) {
+						$taxonomy_parameters[] = array(
+							'taxonomy' => 'category',
+							'field'    => 'term_id',
+							'terms'    => $exclude_ids,
+							'operator' => 'NOT IN',
+						);
+					}
+
+					// if there is a taxonomy query, use it
+					if ( ! empty( $taxonomy_parameters ) ) {
+						$query->set( 'tax_query', $taxonomy_parameters );
+					}
 				}
 			} elseif ( is_author() ) {
+				// for author archives
 				// author archives should not get byline posts
 				$query->set(
 					'meta_query',
@@ -72,7 +113,9 @@ if ( ! function_exists( 'custom_archive_query_vars' ) ) :
 endif;
 
 /**
-* Returns array of grouped categories for the given category
+* Returns array of grouped categories for the given category.
+* This means the categories that are children of a parent category.
+* Ex: all the categories that are "news" categories.
 *
 * @param int $category_id
 * @return array $grouped_categories
@@ -94,6 +137,48 @@ if ( ! function_exists( 'minnpost_get_grouped_categories' ) ) :
 		);
 		$grouped_categories      = get_terms( $grouped_categories_args );
 		return $grouped_categories;
+	}
+endif;
+
+/**
+* Returns the grouping category for the given category.
+* This means the parent category.
+* Ex: returns the "news" category for the "metro" category.
+*
+* @param int $category_id
+* @return array $grouping_category
+*
+*/
+if ( ! function_exists( 'minnpost_get_grouping_category' ) ) :
+	function minnpost_get_grouping_category( $category_id = '' ) {
+		$grouping_field       = '_mp_category_group';
+		$grouping_category_id = get_term_meta( $category_id, $grouping_field, true );
+		$grouping_category    = get_term_by( 'id', $grouping_category_id );
+		return $grouping_category;
+	}
+endif;
+
+/**
+* Returns the grouping category IDs to skip
+* This means the parent categories to skip based on the current category's parent
+* Ex: returns the "opinion" and "arts & culture" and "health" categories for the "metro" category.
+*
+* @param int $category_id
+* @return array $exclude_ids
+*
+*/
+if ( ! function_exists( 'minnpost_get_grouping_categories_to_exclude' ) ) :
+	function minnpost_get_grouping_categories_to_exclude( $category_id = '' ) {
+		$choices     = minnpost_largo_category_groups();
+		$exclude_ids = array();
+		foreach ( $choices as $choice ) {
+			$category = minnpost_largo_group_category( $choice );
+			if ( (int) $category_id !== (int) $category->term_id ) {
+				$exclude_ids[] = array_values( minnpost_get_grouped_categories( $category->term_id ) );
+			}
+		}
+		$exclude_ids = array_merge( ... array_values( $exclude_ids ) );
+		return $exclude_ids;
 	}
 endif;
 
