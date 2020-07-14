e1bf747ba5a8302c40566e4d0a1e6e9d4e7f27d3
Jonathan Stegall <jstegall@minnpost.com>
2020-07-14 17:56:16 -0500

    dark with white letters logo for footer

--- template-parts/logo-footer.php
+++ template-parts/logo-footer.php
@@ -2,7 +2,7 @@
 	<a class="a-logo a-logo-svg" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo get_bloginfo( 'name' ); ?>">
 		<picture>
 			<source type="image/svg+xml" srcset="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/minnpost-logo.svg">
-			<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/logo.png" alt="<?php bloginfo( 'name' ); ?>">
+			<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/logo-bw.png" alt="<?php bloginfo( 'name' ); ?>">
 		</picture>
 	</a>
 </div><!-- .a-site-branding -->
