90824ca84015cc1eaf2bf546d47f3c9eb6aa843b
Jonathan Stegall <jstegall@minnpost.com>
2020-07-14 14:34:48 -0500

    fix the homepage when there is not a top story yet

--- front-page.php
+++ front-page.php
@@ -11,9 +11,9 @@
 
 get_header(); ?>
 	<main id="main" class="site-main m-homepage-zones">
-		<div id="home-list" class="o-homepage-listing">
-			<?php if ( function_exists( 'z_get_zone' ) ) : ?>
-				<?php $top_query = z_get_zone_query( 'homepage-top' ); ?>
+		<?php if ( function_exists( 'z_get_zone' ) ) : ?>
+			<?php $top_query = z_get_zone_query( 'homepage-top' ); ?>
+		<div id="home-list" class="o-homepage-listing<?php if ( $top_query->have_posts() ) : ?> o-homepage-listing-with-top<?php else : ?> o-homepage-listing-without-top<?php endif; ?>">
 				<?php if ( $top_query->have_posts() ) : ?>
 					<?php $post_count = $top_query->post_count; ?>
 					<section class="m-archive m-archive-top m-archive-has-<?php echo (int) $post_count; ?>-post m-zone m-zone-homepage-top">
@@ -26,10 +26,8 @@ get_header(); ?>
 						?>
 						<?php dynamic_sidebar( 'sidebar-glean' ); ?>
 					</section>
+					<?php get_sidebar( 'first' ); ?>
 				<?php endif; ?>
-			<?php endif; ?>
-			<?php get_sidebar( 'first' ); ?>
-			<?php if ( function_exists( 'z_get_zone' ) ) : ?>
 				<?php
 				$top_stories_zone = 'homepage-more-top-stories';
 				$zone             = z_get_zone( $top_stories_zone );
@@ -37,11 +35,21 @@ get_header(); ?>
 				?>
 				<?php if ( $more_top_stories->have_posts() ) : ?>
 					<section class="m-archive m-archive-homepage m-zone m-zone-homepage-more-top">
+						<?php if ( ! $top_query->have_posts() ) : ?>
+							<?php do_action( 'wp_message_inserter', 'above_homepage_articles' ); ?>
+						<?php endif; ?>
 						<h2 class="a-zone-title"><?php echo $zone->description; ?></h2>
 						<?php
+						$count = 1;
 						while ( $more_top_stories->have_posts() ) :
 							$more_top_stories->the_post();
 							get_template_part( 'template-parts/content', 'excerpt' ); // content-middle
+							?>
+							<?php if ( 2 === $count && ! $top_query->have_posts() ) : ?>
+								<?php dynamic_sidebar( 'sidebar-glean' ); ?>
+							<?php endif; ?>
+							<?php
+							$count++;
 						endwhile;
 						?>
 						<!--<div class="a-zone-actions">
@@ -49,10 +57,12 @@ get_header(); ?>
 						</div>-->
 					</section>
 				<?php endif; ?>
-			<?php endif; ?>
-			<?php get_sidebar( 'second' ); ?>
-			<?php do_action( 'wp_message_inserter', 'homepage_middle' ); ?>
-			<?php if ( function_exists( 'z_get_zone' ) ) : ?>
+				<?php if ( ! $top_query->have_posts() ) : ?>
+					<?php get_sidebar( 'first-and-second' ); ?>
+				<?php else : ?>
+					<?php get_sidebar( 'second' ); ?>
+				<?php endif; ?>
+				<?php do_action( 'wp_message_inserter', 'homepage_middle' ); ?>
 				<?php
 				$opinion_zone  = 'homepage-opinion';
 				$zone          = z_get_zone( $opinion_zone );
@@ -72,18 +82,18 @@ get_header(); ?>
 						</div>-->
 					</section>
 				<?php endif; ?>
-			<?php endif; ?>
-			<?php get_sidebar( 'third' ); ?>
-			<div class="m-ad-region m-ad-region-home m-ad-region-home-full-width">
-				<?php do_action( 'acm_tag', 'Middle3' ); ?>
+				<?php get_sidebar( 'third' ); ?>
+				<div class="m-ad-region m-ad-region-home m-ad-region-home-full-width">
+					<?php do_action( 'acm_tag', 'Middle3' ); ?>
+				</div>
+				<div class="m-widget-group m-widget-group-homepage">
+					<?php dynamic_sidebar( 'sidebar-spills' ); ?>
+				</div>
+				<div class="m-widget-group m-widget-group-homepage">
+					<?php dynamic_sidebar( 'sidebar-membership' ); ?>
+				</div>
 			</div>
-			<div class="m-widget-group m-widget-group-homepage">
-				<?php dynamic_sidebar( 'sidebar-spills' ); ?>
-			</div>
-			<div class="m-widget-group m-widget-group-homepage">
-				<?php dynamic_sidebar( 'sidebar-membership' ); ?>
-			</div>
-		</div>
+		<?php endif; ?>
 	</main>
 <?php
 get_footer();
