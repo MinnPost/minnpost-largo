bfe8b969ea86a2742a451068f50b91bfef3e4577
Jonathan Stegall <jstegall@minnpost.com>
2020-07-31 16:59:56 -0500

    sticky zone with less meta

--- template-parts/content-sidebar-with-image.php
+++ template-parts/content-sidebar-with-image.php
@@ -21,24 +21,8 @@
 		);
 	}
 	?>
-	<?php if ( '' === minnpost_get_replace_category_text() ) : ?>
-		<?php minnpost_category_breadcrumb(); ?>
-	<?php else : ?>
-		<?php minnpost_replace_category_text(); ?>
-	<?php endif; ?>
+	
 	<header class="m-entry-header">
 		<h4 class="a-entry-title"><a href="<?php echo get_permalink( get_the_ID() ); ?>"><?php echo get_the_title( get_the_ID() ); ?></a></h4>
-		<?php if ( 'post' === get_post_type( get_the_ID() ) ) : ?>
-			<?php if ( '' !== minnpost_get_posted_by() ) : ?>
-				<div class="m-entry-byline">
-					<?php minnpost_posted_by( get_the_ID() ); ?>
-				</div>
-			<?php endif; ?>
-			<div class="m-entry-meta">
-				<?php if ( '' !== minnpost_get_posted_on() ) : ?>
-					<?php minnpost_posted_on(); ?>
-				<?php endif; ?>
-			</div><!-- .m-entry-meta -->
-		<?php endif; ?>
 	</header>
 </li>
