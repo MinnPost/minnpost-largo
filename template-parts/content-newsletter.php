ceafb2fbcce3dc8a7cd7b6447e4765282b83e44f
Jonathan Stegall <jstegall@minnpost.com>
2020-12-14 15:04:14 -0600

    remove the css from the email template

--- template-parts/content-newsletter.php
+++ template-parts/content-newsletter.php
@@ -8,82 +8,15 @@
  */
 
 ?>
-<style type="text/css">
-	body {
-		Margin: 0; padding: 0; min-width: 100%; background-color: #ffffff;
-	}
-	img {
-		border: 0 none; height: auto; line-height: 100%; outline: none; text-decoration: none;
-	}
-	a:hover {
-		color: #1A1818 !important; text-decoration: underline !important;
-	}
-	a:active {
-		color: #1A1818 !important; text-decoration: underline !important;
-	}
-	a:visited {
-		color: #1A1818 !important;
-	}
-	h3 a:active {
-		color: #1A1818;
-	}
-	h5 a:active {
-		color: #1A1818;
-	}
-	h3 a:visited {
-		color: #1A1818;
-	}
-	h5 a:visited {
-		color: #1A1818;
-	}
-	.content .button.read-story td td a:hover {
-		color: #ffffff !important; text-decoration: none !important;
-	}
-	.content .button.read-story td td a:visited {
-		color: #ffffff !important; text-decoration: none !important;
-	}
-	.content .button.read-story td td a:active {
-		color: #ffffff !important; text-decoration: none !important;
-	}
-	.footer a:hover {
-		color: #801018 !important; text-decoration: underline; !important;
-	}
-	.footer a:visited {
-		color: #801018 !important; text-decoration: none; !important;
-	}
-	.footer a:active {
-		color: #801018 !important; text-decoration: underline; !important;
-	}
-	@media only screen and (max-width: 600px) {
-		.two-column.header .logo td {
-			border-bottom: 10px solid #000 !important; Margin-bottom: 5px !important;
-		}
-		.two-column.header .date {
-			text-align: left !important;
-		}
-		.two-column.header .date td {
-			text-align: left !important;
-		}
-		.one-column .column, .two-column .column, .two-column .column.ad, .two-column .column.more, .two-column .column.date, .two-column .column.logo {
-			max-width: 90% !important; Margin-right: auto !important; Margin-left: auto !important;
-		}
-		.content.story .ad {
-			border-bottom: 2px solid #cccccf !important; padding-bottom: 18px !important; Margin-bottom: 18px !important;
-		}
-		div.story.story-last .story-inner {
-			border-bottom: 2px solid #cccccf !important; padding-bottom: 18px !important; Margin-bottom: 18px !important;
-		}
-	}
-</style>
 
-<center class="wrapper" style="-ms-text-size-adjust: 100%; -webkit-text-size-adjust: 100%; table-layout: fixed; width: 100%">
+<center class="wrapper">
 	<div class="webkit">
 		<!--[if (gte mso 9)|(IE)]>
 			<table cellpadding="0" cellspacing="0" width="600" align="center">
 				<tr>
 					<td>
 		<![endif]-->
-		<table cellpadding="0" cellspacing="0" class="outer" align="center" style="border-collapse: collapse; border-spacing: 0; color: #1a1818; font-family: Helvetica, Arial, Geneva, sans-serif; Margin: 0 auto; max-width: 600px; mso-table-lspace: 0pt; mso-table-rspace: 0pt; padding: 0; width: 100%; background-color: #ffffff;">
+		<table cellpadding="0" cellspacing="0" class="outer" align="center">
 			<tr>
 				<td class="two-column header" style="border-bottom-color: #000; border-bottom-style: solid; border-bottom-width: 10px; border-collapse: collapse; font-size: 0; Margin: 0; padding: 0; text-align: center" align="center">
 					<!--[if (gte mso 9)|(IE)]>
