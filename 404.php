00556bc357669d5e9d950439b972c3a48ca84ff5
Jonathan Stegall <jstegall@minnpost.com>
2021-01-20 10:24:18 -0600

    make the 404 template not redundant

--- 404.php
+++ 404.php
@@ -11,20 +11,7 @@ get_header(); ?>
 
 	<div id="primary" class="m-layout-primary">
 		<main id="main" class="site-main">
-
-			<?php
-			$query = new WP_Query(
-				array(
-					'post_type' => 'page',
-					'pagename'  => 'page-not-found',
-				)
-			);
-			while ( $query->have_posts() ) : $query->the_post();
-				get_template_part( 'template-parts/content', 'page' );
-			endwhile; // End of the loop.
-			wp_reset_postdata();
-			?>
-
+			<?php get_template_part( 'template-parts/content', 'none' ); ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
