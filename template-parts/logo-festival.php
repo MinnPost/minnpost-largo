369412ed3e52a90348a59c82db69776da774e168
Jonathan Stegall <jstegall@minnpost.com>
2021-01-27 13:50:03 -0600

    logo css for festival

--- template-parts/logo-festival.php
+++ template-parts/logo-festival.php
@@ -2,16 +2,18 @@
 $festival_logo_info = minnpost_largo_get_festival_logo_info( 'festival' );
 ?>
 
-<div class="m-festival-logo">
-	<div class="a-festival-branding<?php echo $festival_logo_info['is_current_url'] ? ' a-festival-branding-landing-page' : ''; ?>">
-		<a href="<?php echo $festival_logo_info['url']; ?>"><?php echo $festival_logo_info['title']; ?></a>
-	</div>
-	<?php
-	if ( function_exists( 'minnpost_largo_get_festival_date_range' ) ) :
-		$date = minnpost_largo_get_festival_date_range( 'sessions' );
-		?>
-		<div class="a-festival-dates">
-			<?php echo $date; ?>
+<div class="m-festival-branding<?php echo $festival_logo_info['is_current_url'] ? ' m-festival-branding-landing-page' : ''; ?>">
+	<a href="<?php echo $festival_logo_info['url']; ?>">
+		<div class="a-festival-branding">
+			<?php echo $festival_logo_info['title']; ?>
 		</div>
-	<?php endif; ?>
+		<?php
+		if ( function_exists( 'minnpost_largo_get_festival_date_range' ) ) :
+			$date = minnpost_largo_get_festival_date_range( 'sessions' );
+			?>
+			<div class="a-festival-dates">
+				<?php echo $date; ?>
+			</div>
+		<?php endif; ?>
+	</a>
 </div>
