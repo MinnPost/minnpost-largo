b017ea662c2054efc09c5178c932cfc9b70ac145
Jonathan Stegall <jstegall@minnpost.com>
2021-06-21 13:23:30 -0500

    mc preview text

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -1163,6 +1163,12 @@ if ( ! function_exists( 'email_preview_text' ) ) :
 			&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
 			</div>
 			<?php
+		else:
+			?>
+			<!--*|IF:MC_PREVIEW_TEXT|*-->
+			<!--[if !gte mso 9]><!----><span class="mcnPreviewText" style="display:none; font-size:0px; line-height:0px; max-height:0px; max-width:0px; opacity:0; overflow:hidden; visibility:hidden; mso-hide:all;">*|MC_PREVIEW_TEXT|*</span><!--<![endif]--> 
+			<!--*|END:IF|*-->
+			<?php
 		endif;
 	}
 endif;
