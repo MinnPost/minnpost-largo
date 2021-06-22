054358aee15ea266146f414766e90f437a3e56cf
Jonathan Stegall <jstegall@minnpost.com>
2021-06-22 09:33:43 -0500

    try to do some manual html minifying

--- header-newsletter.php
+++ header-newsletter.php
@@ -16,46 +16,21 @@
 		<meta name="viewport" content="width=device-width,initial-scale=1">
 		<meta name="x-apple-disable-message-reformatting">
 		<?php if ( false === $args['is_legacy'] ) : ?>
-			<meta name="color-scheme" content="light dark">
-			<meta name="supported-color-schemes" content="light dark">
-			<style_donotremove>
-			:root {
-				color-scheme: light dark;
-				supported-color-schemes: light dark;
-			}
-			u + #body a {
-				color: inherit !important;
-				text-decoration: none !important;
-				font-size: inherit !important;
-				font-family: inherit !important;
-				font-weight: inherit !important;
-				line-height: inherit !important;
-			}
-			#MessageViewBody a {
-				color: inherit !important;
-				text-decoration: none !important;
-				font-size: inherit !important;
-				font-family: inherit !important;
-				font-weight: inherit !important;
-				line-height: inherit !important;
-			}
-			</style_donotremove>
+		<meta name="color-scheme" content="light dark">
+		<meta name="supported-color-schemes" content="light dark">
+		<style_donotremove>
+		:root{color-scheme:light dark;supported-color-schemes:light dark;}u+#body a{color:inherit!important;text-decoration:none!important;font-size:inherit!important;font-family:inherit!important;font-weight:inherit!important;line-height:inherit!important;}#MessageViewBody a{color:inherit!important;text-decoration:none!important;font-size:inherit!important;font-family:inherit!important;font-weight:inherit!important;line-height:inherit!important;}
+		</style_donotremove>
 		<?php endif; ?>
 		<link rel="shortcut icon" href="<?php echo esc_url( get_theme_file_uri( '/assets/img/app-icons/favicon.ico' ) ); ?>" type="image/x-icon" />
 		<meta property="og:title" content="<?php echo get_the_title(); ?> | <?php bloginfo( 'name' ); ?>">
 		<title><?php echo get_the_title(); ?> | <?php bloginfo( 'name' ); ?></title>
 		<!--[if mso]>
 		<style type="text/css">
-			table {border-collapse:collapse;border-spacing:0;margin:0;}
-			div, td {padding:0;}
-			div {margin:0 !important;}
+		table{border-collapse:collapse;border-spacing:0;margin:0;}div,td{padding:0;}div{margin:0!important;}
 		</style>
 		<noscript>
-			<xml>
-				<o:OfficeDocumentSettings>
-					<o:PixelsPerInch>96</o:PixelsPerInch>
-				</o:OfficeDocumentSettings>
-			</xml>
+			<xml><o:OfficeDocumentSettings><o:PixelsPerInch>96</o:PixelsPerInch></o:OfficeDocumentSettings></xml>
 		</noscript>
 		<![endif]-->
 	</head>
