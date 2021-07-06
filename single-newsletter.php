ba3fdaa68ec220286bac8e68e6ca2739f07cb975
Jonathan Stegall <jstegall@minnpost.com>
2021-07-06 11:41:14 -0500

    note the outlook issue

--- single-newsletter.php
+++ single-newsletter.php
@@ -57,7 +57,7 @@ if ( false === $is_legacy ) {
 
 	// remove newlines.
 	$html = preg_replace( "/\n/", '', $html );
-	// remove spaces between HTML tags.
+	// remove spaces between HTML tags. this causes issues for Outlook on windows, apparently.
 	//$html = preg_replace( '/>\s*</', '><', $html );
 	// wrap the HTML at 500 characters for email clients.
 	$html = wordwrap( $html, 500 );
