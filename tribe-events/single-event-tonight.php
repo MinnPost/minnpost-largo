764604d9a6836668011a49e3b98d2c606083fabf
Jonathan Stegall <jstegall@minnpost.com>
2021-08-11 12:48:55 -0500

    mainly mobile fixes for mp tonight

--- tribe-events/single-event-tonight.php
+++ tribe-events/single-event-tonight.php
@@ -1,22 +1,22 @@
 <?php
 /**
- * The template for displaying the MinnPost Festival single event
+ * The template for displaying the MinnPost Tonight single event
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
  * @package MinnPost Largo
  */
-get_header( 'festival' ); ?>
+get_header( 'tonight' ); ?>
 
-	<div id="primary" class="m-layout-primary o-festival o-festival-single">
+	<div id="primary" class="m-layout-primary o-tonight o-tonight-single">
 		<main id="main" class="site-main">
 			<?php
 			while ( have_posts() ) :
 				the_post();
-				get_template_part( 'template-parts/post-festival', 'tribe_events' );
+				get_template_part( 'template-parts/post-tonight', 'tribe_events' );
 			endwhile; // End of the loop.
 			?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 <?php
-get_footer( 'festival' );
+get_footer( 'tonight' );
