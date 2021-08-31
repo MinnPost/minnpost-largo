10b30c02bb615d9cf1aa1b9dcfb5676dcee27ad3
Jonathan Stegall <jstegall@minnpost.com>
2021-08-31 16:18:22 -0500

    mp tonight speakers don't need to show the sessions

--- template-parts/post-tribe_ext_speaker-tonight.php
+++ template-parts/post-tribe_ext_speaker-tonight.php
@@ -16,31 +16,6 @@ $speaker_instance = Tribe__Extension__Speaker_Linked_Post_Type::instance();
 		<div class="m-archive-info m-author-info m-author-excerpt">						   
 			<?php minnpost_speaker_figure( get_the_ID(), 'full', 'content', true, 'the_title', true, false, '_tribe_ext_speaker_title', true, true ); ?>
 		</div>
-		<?php $events = $speaker_instance->get_events( get_the_ID() ); ?>
-		<?php if ( $events->have_posts() ) : ?>
-			<section class="m-archive m-archive-events tribe-events-calendar-list">
-				<h2>
-				<?php
-				echo sprintf(
-					// translators: 1) speaker name
-					__( 'MinnPost Tonight sessions with %1$s', 'minnpost-largo' ),
-					get_the_title()
-				);
-				?>
-				</h2>
-				<?php
-				$content_display_args = array(
-					'use_permalink' => true,
-				);
-				while ( $events->have_posts() ) {
-					$events->the_post();
-					set_query_var( 'current_post', $events->current_post );
-					get_template_part( 'template-parts/post-tonight', get_post_type() . '-excerpt', $content_display_args );
-				}
-				wp_reset_postdata();
-				?>
-			</section>
-		<?php endif; ?>
 		<?php minnpost_event_website_disclaimer_text( 'tonight' ); ?>
 	</div>
 </article>
