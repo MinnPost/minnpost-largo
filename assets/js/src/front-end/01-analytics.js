3bcbf2cd11f020000be275425aa9550d631fee4e
Jonathan Stegall <jstegall@minnpost.com>
2022-04-08 13:41:52 -0500

    add gtag support to the basic analytics event method

--- assets/js/src/front-end/01-analytics.js
+++ assets/js/src/front-end/01-analytics.js
@@ -5,24 +5,49 @@
  *
  */
 
-function mpAnalyticsTrackingEvent( type, category, action, label, value ) {
-	if ( typeof gtag !== 'undefined' ) {
+ function mpAnalyticsCheckAnalyticsVersion() {
+	var version = '';
+	if ( 'undefined' !== typeof analytics_tracking_settings && 'undefined' !== typeof analytics_tracking_settings.analytics_type ) {
+		if ( 'gtagjs' === analytics_tracking_settings.analytics_type && 'function' === typeof gtag ) {
+			version = 'gtag';
+		} else if ( 'analyticsjs' === analytics_tracking_settings.analytics_type && 'function' === typeof ga ) {
+			version = 'ga';
+		}
+	}
+	return version;
+}
+
+function mpAnalyticsTrackingEvent( type, category, action, label, value, non_interaction ) {
+	var version = mpAnalyticsCheckAnalyticsVersion();
+	if ( 'gtag' === version ) {
+		// Sends the event to the Google Analytics property with
+		// tracking ID GA_MEASUREMENT_ID set by the config command in
+		// the global tracking snippet.
+		// example: gtag('event', 'play', { 'event_category': 'Videos', 'event_label': 'Fall Campaign' });
 		var params = {
 			'event_category': category,
 			'event_label': label
 		};
-		if ( typeof value !== 'undefined' ) {
+		if ( 'undefined' !== typeof value ) {
 			params.value = value;
 		}
+		if ( 'undefined' !== typeof non_interaction ) {
+			params.non_interaction = non_interaction;
+		}
 		gtag( type, action, params );
-	} else if ( typeof ga !== 'undefined' ) {
+	} else if ( 'ga' === version ) {
+		// Uses the default tracker to send the event to the
+		// Google Analytics property with tracking ID GA_MEASUREMENT_ID.
+		// example: ga('send', 'event', 'Videos', 'play', 'Fall Campaign');
+		// noninteraction seems to have been working like this in analytics.js.
+		if ( non_interaction == 1 ) {
+			value = { 'nonInteraction': 1 };
+		}
 		if ( 'undefined' === typeof value ) {
 			ga( 'send', type, category, action, label );
 		} else {
 			ga( 'send', type, category, action, label, value );
 		}
-	} else {
-		return;
 	}
 }
 
