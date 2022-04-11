1bc2e18d73f755b645c5c0ab8de65a0d5ba28121
Jonathan Stegall <jstegall@minnpost.com>
2022-04-11 14:39:52 -0500

    use wp hooks for the analytics plugin

--- assets/js/src/front-end/01-analytics.js
+++ assets/js/src/front-end/01-analytics.js
@@ -5,52 +5,30 @@
  *
  */
 
- function mpAnalyticsCheckAnalyticsVersion() {
-	var version = '';
-	if ( 'undefined' !== typeof analytics_tracking_settings && 'undefined' !== typeof analytics_tracking_settings.analytics_type ) {
-		if ( 'gtagjs' === analytics_tracking_settings.analytics_type && 'function' === typeof gtag ) {
-			version = 'gtag';
-		} else if ( 'analyticsjs' === analytics_tracking_settings.analytics_type && 'function' === typeof ga ) {
-			version = 'ga';
-		}
-	}
-	return version;
+/*
+ * Call hooks from other places.
+ * This allows other plugins that we maintain to pass data to the theme's analytics method.
+*/
+if ( typeof wp !== 'undefined' ) {
+	wp.hooks.addAction( 'wpMessageInserterAnalyticsEvent', 'minnpostLargo', mpAnalyticsTrackingEvent, 10 );
 }
 
+/*
+ * Create a Google Analytics event for the theme. This calls the wp-analytics-tracking-generator action.
+ * type: generally this is "event"
+ * category: Event Category
+ * label: Event Label
+ * action: Event Action
+ * value: optional
+ * non_interaction: optional
+*/
 function mpAnalyticsTrackingEvent( type, category, action, label, value, non_interaction ) {
-	var version = mpAnalyticsCheckAnalyticsVersion();
-	if ( 'gtag' === version ) {
-		// Sends the event to the Google Analytics property with
-		// tracking ID GA_MEASUREMENT_ID set by the config command in
-		// the global tracking snippet.
-		// example: gtag('event', 'play', { 'event_category': 'Videos', 'event_label': 'Fall Campaign' });
-		var params = {
-			'event_category': category,
-			'event_label': label
-		};
-		if ( 'undefined' !== typeof value ) {
-			params.value = value;
-		}
-		if ( 'undefined' !== typeof non_interaction ) {
-			params.non_interaction = non_interaction;
-		}
-		gtag( type, action, params );
-	} else if ( 'ga' === version ) {
-		// Uses the default tracker to send the event to the
-		// Google Analytics property with tracking ID GA_MEASUREMENT_ID.
-		// example: ga('send', 'event', 'Videos', 'play', 'Fall Campaign');
-		// noninteraction seems to have been working like this in analytics.js.
-		if ( non_interaction == 1 ) {
-			value = { 'nonInteraction': 1 };
-		}
-		if ( 'undefined' === typeof value ) {
-			ga( 'send', type, category, action, label );
-		} else {
-			ga( 'send', type, category, action, label, value );
-		}
-	}
+	wp.hooks.doAction('wpAnalyticsTrackingGeneratorEvent', type, category, action, label, value, non_interaction);
 }
 
+/*
+ * When a part of the website is member-specific, create an event for whether it was shown or not.
+*/
 document.addEventListener( 'DOMContentLoaded', function( event ) {
 	if ( 'undefined' !== typeof minnpost_membership_data && '' !== minnpost_membership_data.url_access_level ) {
 		var type = 'event';
